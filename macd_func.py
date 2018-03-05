4806941f61f4aac003218721d35466ce3d0a1fe1
xiaoshenke <1205914137@qq.com>
2018-03-05 16:48:46 +0800

    update macd_func

--- macd_func.py
+++ macd_func.py
@@ -30,27 +30,29 @@ def movingaverage(values,window):
 
 MA1 = 5
 MA2 = 10
+EMA = 9
 
 def create_dataframe(func):
 	index = pd.date_range('2017-02-01','2017-04-01',freq='1D')
 	df = DataFrame({'kdata':np.arange(1,len(index)+1)},index=index)
 	df.kdata = df.kdata.map(func)
-	#df.index.name='date'
 	df.reset_index(inplace=True)
 	df.columns=['date','kdata']
 	return df
 
-# TODO
-def draw_macd():
-	return
-
-# TODO
 def draw_func(func):
 	df = create_dataframe(func)
-	#print df
+	
 	Av1 = movingaverage(df.kdata.values,MA1)
 	Av2 = movingaverage(df.kdata.values,MA2)
 	SP = len(df.kdata.values[MA2-1:])
+	
+	av1_series = Series(Av1,index=df.index[MA1-1:])
+	av1_series.name = 'av1'
+	av2_series = Series(Av2,index=df.index[MA2-1:])
+	av2_series.name = 'av2'
+	df['av1'] = av1_series
+	df['av2'] = av2_series
 	
 	fig = plt.figure(facecolor='#07000d',figsize=(15,10))
 	ax1 = plt.subplot2grid((6,4), (1,0), rowspan=4, colspan=4, axisbg='#07000d')
@@ -70,13 +72,28 @@ def draw_func(func):
 	ax1.tick_params(axis='x', colors='w')	
 	# draw y-axis
 	ax1.tick_params(axis='y', colors='w')   
+
+	df['macd'] = df['av1'] - df['av2']
+	Eav = movingaverage(df.macd.values,EMA)
+	
+	# fixme: why MA2-2?
+	eva_series = Series(Eav,index=df.index[MA2-2:])
+	df['ema'] = eva_series
+
+	ax2 = plt.subplot2grid((6,4), (5,0), sharex=ax1, rowspan=4, colspan=4, axisbg='#07000d')
+	ax2.plot(df.date.values[-SP:], df.macd[-SP:], color='#4ee6fd', lw=2)
+	ax2.plot(df.date.values[-SP:], df.ema[-SP:], color='#e1edf9', lw=1)
+	ax2.tick_params(axis='x', colors='w')
+	ax2.tick_params(axis='y', colors='w')
 	plt.show()
+
+	print df
 	return
 
 
 def main():
-	draw_func(partial(linear_func,0.5,0))
-	#draw_func(partial(parabolic_func,-2,5,50))
+	#draw_func(partial(linear_func,0.5,0))
+	draw_func(partial(parabolic_func,-0.03,40,50))
 	return
 
 # Used to explain how macd works 
