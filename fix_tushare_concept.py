7812a545866e9b4f46d7a51c66df6e02e78affa9
xiaoshenke <1205914137@qq.com>
2018-04-03 16:48:09 +0800

    sort file

---
+++ fix_tushare_concept.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# tushare.get_concept_classified() is not working,so fix this interface
+# TODO:
+
---
+++ smart_stock.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# 根据价格位置,市场情绪,板块数据选股
+
+
