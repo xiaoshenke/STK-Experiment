3981c3d390e3252d861c11e2a4017adf0e658170
xiaoshenke <1205914137@qq.com>
2018-05-11 13:59:28 +0800

    add update_dapan_stock

--- time_util.py
+++ time_util.py
@@ -2,18 +2,13 @@
 
 import datetime
 
-# return datetime
-def get_today_of_last_year():
-	now = datetime.datetime.now()
-	return datetime.datetime(now.year-1,now.month,now.day,now.hour,now.minute,now.second)
+def today_last_year():
+    lasty = today() + datetime.timedelta(-365)
+    return lasty
 
-def get_today_of_last_year_formatted():
-	day = get_today_of_last_year()
-	return day.strftime('%Y-%m-%d')
-
-def get_today_formatted():
-	return datetime.datetime.now().strftime('%Y-%m-%d')
+def today():
+    day = datetime.datetime.today().date()
+    return day
 
 if __name__ == "__main__":
-	print get_today_of_last_year()
-
+	pass
