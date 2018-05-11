3de922e73f09f1ac7a0047b25575edc7b032ab0d
xiaoshenke <1205914137@qq.com>
2018-05-11 22:08:18 +0800

    add cxg_stat

--- cons.py
+++ cons.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/csv_data/"
-CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/csv_data/"
+CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/csv_data/"
+#CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/csv_data/"
 
 IS_DEBUG=True
 #IS_DEBUG=False
