8298190986b2bf6f9dcb41fce147ef3ba25139d4
xiaoshenke <1205914137@qq.com>
2018-05-14 17:15:24 +0800

    add cal_indexs_of

--- time_util.py
+++ time_util.py
@@ -2,6 +2,10 @@
 
 import datetime
 
+# return:date
+def parse_date_str(origin):
+	return datetime.datetime.strptime(origin,'%Y-%m-%d').date()
+
 def today_last_year():
     lasty = today() + datetime.timedelta(-365)
     return lasty
