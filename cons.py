3ffd8976a07189932547e3365d3bb549569daff4
xiaoshenke <1205914137@qq.com>
2018-06-05 12:48:45 +0800

    add guess_and_check_2

--- cons.py
+++ cons.py
@@ -7,8 +7,8 @@ CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/csv_data/"
 # 这里的IS_DEBUG用于控制load_csv里的信息 相当于在原本的logger的debug下又加了一层
 # 目前的logger级别: IS_DEBUG=True + debug,IS_DEBUG=False + debug,info,warn,error
  
-#IS_DEBUG=True
-IS_DEBUG=False
+IS_DEBUG=True
+#IS_DEBUG=False
 
 IS_TEST=True
 #IS_TEST=False
