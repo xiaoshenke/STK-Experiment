622c315130e92ce9f9dbb1f10f38a4a0746e525a
xiaoshenke <1205914137@qq.com>
2018-07-23 00:16:50 +0800

    finish stk_break_xt

--- cons.py
+++ cons.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # coding=utf-8
 
-CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/csv_data/"
-#CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/csv_data/"
+#CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/csv_data/"
+CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/csv_data/"
 
 # 这里的IS_DEBUG用于控制load_csv里的信息 相当于在原本的logger的debug下又加了一层
 # 目前的logger级别: IS_DEBUG=True + debug,IS_DEBUG=False + debug,info,warn,error
@@ -12,3 +12,7 @@ IS_DEBUG=False
 
 IS_TEST=True
 #IS_TEST=False
+
+# to run this, export PYTHONPATH=/Users/dashu/Desktop/Playgroud/STK-Experiment:$PYTHONPATH
+# to run this, export PYTHONPATH=/Users/wuxian/Desktop/STK-Experiment:$PYTHONPATH
+
