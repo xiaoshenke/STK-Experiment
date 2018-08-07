6063839af5aff99061595e9ac8206fbc45c26105
xiaoshenke <1205914137@qq.com>
2018-08-08 00:35:53 +0800

    update

--- cons.py
+++ cons.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/data/csv_data/"
-CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/data/csv_data/"
+CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/data/csv_data/"
+#CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/data/csv_data/"
 
-CUR_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment"
+#CUR_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment"
+CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 
 # 这里的IS_DEBUG用于控制load_csv里的信息 相当于在原本的logger的debug下又加了一层
 # 目前的logger级别: IS_DEBUG=True + debug,IS_DEBUG=False + debug,info,warn,error
