d4e641e82d5060a369284179f5c5cc93eb751f55
xiaoshenke <1205914137@qq.com>
2018-08-08 17:24:58 +0800

    add strategy.filter

--- cons.py
+++ cons.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#CSV_PATH="/Users/wuxian/Desktop/STK-Experiment/data/csv_data/"
-CSV_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment/data/csv_data/"
-
 CUR_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment"
 #CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 
-# 这里的IS_DEBUG用于控制load_csv里的信息 相当于在原本的logger的debug下又加了一层
-# 目前的logger级别: IS_DEBUG=True + debug,IS_DEBUG=False + debug,info,warn,error
- 
-#IS_DEBUG=True
-IS_DEBUG=False
-
-IS_TEST=True
-#IS_TEST=False
+CSV_PATH="%s/data/csv_data/"%CUR_PATH
 
 # to run this, export PYTHONPATH=/Users/dashu/Desktop/Playgroud/STK-Experiment:$PYTHONPATH
 # to run this, export PYTHONPATH=/Users/wuxian/Desktop/STK-Experiment:$PYTHONPATH
--- util/load_memory.py
+++ util/load_memory.py
@@ -2,20 +2,13 @@
 # coding=utf-8
 # 从内存加载结果装饰器
 
-from cons import IS_DEBUG
 from log import logger
 
-STOCK = 1
-INDEX = 2
-PLATE = 3
-AREA  = 4
-TOVER = 5
+DEBUG_OPEN = False
 
-stk_map = {}
-index_map = {}
-plate_map = {}
-area_map = {}
-tover_map = {}
+STOCK = 1;INDEX = 2;PLATE = 3;AREA  = 4;TOVER = 5
+
+stk_map = {};index_map = {};plate_map = {};area_map = {};tover_map = {}
 
 def load_memory(code,type=STOCK):
 	def _load_memory(func):
@@ -32,15 +25,15 @@ def load_memory(code,type=STOCK):
 			elif type == TOVER:
 				r_map = tover_map			
 			else:
-				if IS_DEBUG:
+				if DEBUG_OPEN:
 					logger.debug("@load_memory,type:%s not supported,ignore decrator"%type)
 				return func(*args,**kwargs)
 
 			if code in r_map:
-				if IS_DEBUG:
+				if DEBUG_OPEN:
 					logger.debug("@load_memory hit memory")
 				return r_map[code]
-			if IS_DEBUG:
+			if DEBUG_OPEN:
 				logger.debug("@load_memory hit memory fail")
 
 			ret = func(*args,**kwargs)
