3bd2a8cd049f4e5d1e08c41e59e98b35f7cd7bfe
xiaoshenke <1205914137@qq.com>
2018-08-23 14:00:10 +0800

    add get_unusual_volume

--- sh/lock.sh
+++ sh/lock.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+for csv in `ls | grep [.]csv`
+do
+	echo python cmd.py encypt_df_file $csv
+	python cmd.py encypt_df_file $csv
+done
+
+
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
--- sh/unlock.sh
+++ sh/unlock.sh
@@ -12,3 +12,9 @@ echo $sec > secret
 
 sh sh/uncrpyt.sh
 
+for csv in `ls | grep [.]csv`
+do
+        echo python cmd.py decrypt_df_file $csv
+        python cmd.py decrypt_df_file $csv
+done
+
