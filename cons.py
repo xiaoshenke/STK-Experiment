bad2ab82da021f7a4c11f30b10f17418e31e8266
xiaoshenke <1205914137@qq.com>
2018-09-12 14:04:09 +0800

    finish huanshou_long

--- cons.py
+++ cons.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#CUR_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment"
-CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
+CUR_PATH="/Users/dashu/Desktop/Playgroud/STK-Experiment"
+#CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 
 CSV_PATH="%s/data/csv_data/"%CUR_PATH
 
