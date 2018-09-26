d7e8d9d9ce161dafda047593b77e0f109372fa68
xiaoshenke <1205914137@qq.com>
2018-09-26 19:41:54 +0800

    update

--- util/time_util.py
+++ util/time_util.py
@@ -2,6 +2,19 @@
 
 import datetime
 from log import logger
+import calendar
+
+def get_month_start(month,year=2018):
+	return return '%d-%02d-01'%(year,month)
+
+#def get_month_start(day):
+#	day = parse_date_str(day)
+#	return '%d-%02d-01'%(day.year,day.month)
+
+def get_month_end(day):
+	day = parse_date_str(day)
+	wday,monthRange = calendar.monthrange(day.year, day.month)
+	return '%d-%02d-%02d'%(day.year,day.month,monthRange)
 
 # return:date
 def parse_date_str(origin):
@@ -20,4 +33,7 @@ def next_day(day):
 
 def last_day(day):
 	return str(parse_date_str(day)+datetime.timedelta(-1))
+
+if __name__ == "__main__":
+	print get_month_end('2018-02-03')
 
