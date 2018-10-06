0e8d24a5f4a6ed6d537fd061ffa7238aba807cf9
xiaoshenke <1205914137@qq.com>
2018-10-06 10:46:51 +0800

    add benchmark.cd.with_brk

--- sh/ls_and_copy.sh
+++ sh/ls_and_copy.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 # Usage ./ls_and_copy.sh your-code
 
+is_week=0
+
+while [ -n "$1" ]
+do
+        case "$1" in
+        -w)
+		is_week=1	
+		;;
+        *)
+                break
+                ;;
+        esac
+	shift
+done
+
 if [ $# -ne 1 ]
 then
         echo Usage: ./ls_and_copy.sh your-code
         exit 2
 fi
 
-ls |grep $1 |xargs sh sh/copy_to.sh
-
+if [ $is_week -eq 0 ]
+then
+	ls |grep $1 |xargs sh sh/copy_to.sh
+else
+	ls |grep $1 |grep w_ |xargs sh sh/copy_to.sh
+fi
 
