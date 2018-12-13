123de46cec54de4bcea5213977ccc6d3b46f8585
xiaoshenke <1205914137@qq.com>
2018-12-13 11:02:10 +0800

    add tool.show_max_up_pchg_recent

--- cons.py
+++ cons.py
@@ -4,6 +4,7 @@
 CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 CSV_PATH="%s/data/csv_data/"%CUR_PATH
 XLS_PATH="%s/data/xls_data/"%CUR_PATH
+TOOL_PATH="%s/data/tool/"%CUR_PATH
 
 STRA_DATA_PATH="%s/data/strategy/"%CUR_PATH
 
