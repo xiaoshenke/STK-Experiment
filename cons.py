b54d3d39ee5ea37e59991238ac9509656f34526e
xiaoshenke <1205914137@qq.com>
2019-02-15 14:44:15 +0800

    restructure inn framework runing saved files

--- cons.py
+++ cons.py
@@ -5,12 +5,22 @@ CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 CSV_PATH="%s/data/csv_data/"%CUR_PATH
 XLS_PATH="%s/data/xls_data/"%CUR_PATH
 TOOL_PATH="%s/data/tool/"%CUR_PATH
-INN_DATA_PATH="%s/data/inn_strategy/"%CUR_PATH
 REPORT_PATH="%s/data/report/"%CUR_PATH
 
-INN_TMP="%s/data/inn_tmp/"%CUR_PATH
-INN_RECORD="%s/data/inn_record/"%CUR_PATH
-INN_RUNNING="%s/data/inn_running/"%CUR_PATH
+# inn.apply()返回的文件存储路径 -> 当前的功能其实..是下载远程文件
+INN_APPLY="%s/data/inn/apply/"%CUR_PATH
+
+# evaluater.evaluate()返回的文件存储路径 -> 实际的计算结果落地(其实没啥用,并不觉得需要存储->因为已经有了上面的下载文件,复盘的时候重新算就好了)
+INN_EVALUATE="%s/data/inn/evaluate/"%CUR_PATH
+
+# 每日的运行日志以及ignore codes(当前并没有启用)
+INN_RUNNING="%s/data/inn/running/"%CUR_PATH
+
+# 每日通过standby下载的所有文件路径
+INN_FENSHI_CODES="%s/data/inn/fenshi_wholecodes/"%CUR_PATH
+
+# 每日策略的filter source文件路径
+INN_FILTER_SOURCE="%s/data/inn/filter_source/"%CUR_PATH
 
 DEBUG_PATH="%s/data/debug/"%CUR_PATH
 
