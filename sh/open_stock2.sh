021d24d5a4459e9c30a2a8198c6c1d586c914575
xiaoshenke <1205914137@qq.com>
2019-02-21 22:46:00 +0800

    add new open_stock.sh and do some update

--- sh/open_stock2.sh
+++ sh/open_stock2.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-# Usage: ./open_stock.sh -d|-w|-f -i [-ig] your-code
+#!/bin/bash 
+# Usage: ./open_stock.sh -d[k]|-w[k]|-m[k]|-f -i[mg]|-w [-ig] your-code 
+# -f:fenshitu -i:open img -w:open website(default)
+# -dk day kline,-wk week kline,-mk month kline
 
 # open shangzheng index, sh/open_stock.sh -index 000001
 # open shenzheng index, sh/open_stock.sh -f -i 399001
 # open chuangye index,sh/open_stock.sh 399006
 
+JPG_PATH="data/jpg_data"
+
 # 1:open website 2:open img
 open_type=1
+
 # 1:dk 2:wk 3:mk 4:f
 stock_type=1
-# 0:not index 1:index
 is_index=0
 ignore_local=0
-
-appender="d"
-
-# extract parameters
 while [ -n "$1" ]
 do
 	case "$1" in
 	-h)
-		echo Usage: sh/open_stock.sh "-d[k]|-w[k]|-f -i[mg]|-w -index" your-code
+		echo Usage: sh/open_stock.sh "-d[k]|-w[k]|-m[k]|-f -i[mg]|-w -index" your-code
 		echo '''for eg. 
 open shangzheng index, sh/open_stock.sh -index 000001
 open shenzheng index, sh/open_stock.sh 399001'''
@@ -28,21 +28,24 @@ open shenzheng index, sh/open_stock.sh 399001'''
 		;;
 	-d | -dk)
 		stock_type=1
-		appender="d"
 		;;
 	-w | -wk)
 		stock_type=2
-		appender="w"
+		;;
+	-m | -mk)
+		stock_type=3
 		;;
 	-f)
 		stock_type=4
-		appender="f"
 		;;
 	-i | -img)
 		open_type=2
 		;;
 	-index)
 		is_index=1
+		;;
+	-w)
+		open_type=1
 		;;
 	-ig)
 		ignore_local=1
@@ -58,13 +61,14 @@ open shenzheng index, sh/open_stock.sh 399001'''
 	shift
 done
 
+#echo stock_type:$stock_type
+
 if [ $# -ne 1 ]
 then
 	echo Usage:./open_stock.sh -d[k]|-w[k]|-m[k]|-f -i[mg]|-w your-code
 	exit 2
 fi
 
-origin_code=$1
 code=$1
 if [ $is_index -eq 1 ] || [[ ${code:0:2} == "60" ]]
 then
@@ -73,7 +77,6 @@ else
 	code=sz$code
 fi
 
-# if it is open browser,just open it
 if [ $open_type -eq 1  ]
 then
 	url=http://finance.sina.com.cn/realstock/company/$code/nc.shtml
@@ -82,25 +85,31 @@ then
 	exit 2
 fi
 
-# else,we will download and open image,carefully deal with -ignore parameter
-
 url=""
-local_code=$origin_code.$appender.jpg
-
+local=""
+local_code=""
 if [ $stock_type -eq 1 ]
 then
 	url=http://image.sinajs.cn/newchart/daily/n/$code.gif
+	local=`find $JPG_PATH|grep $code.jpg|head -n 1`
+	local_code=$code.jpg
 elif [ $stock_type -eq 2 ]
 then
 	url=http://image.sinajs.cn/newchart/weekly/n/$code.gif
+	local=`find $JPG_PATH|grep w_$code.jpg|head -n 1`
+	local_code=w_$code.jpg
 elif [ $stock_type -eq 3 ]
 then
 	url=http://image.sinajs.cn/newchart/monthly/n/$code.gif
+	local=`find $JPG_PATH|grep m_$code.jpg|head -n 1`
+	local_code=m_$code.jpg
 else
 	url=http://image.sinajs.cn/newchart/min/n/$code.gif
+	local=`find $JPG_PATH|grep $code.jpg|head -n 1`
+	local_code=$code.jpg
 fi
 
-# check whether code is already exist
+# ????? don't understand..
 if [ -f $local_code ] && [ $ignore_local -eq 0 ]
 then
 	echo $local_code already exist!
@@ -108,8 +117,17 @@ then
 	exit 2
 fi
 
-# finally download and open
+if [ ${#local} -gt 6 ] && [ $ignore_local -eq 0 ]
+then
+	echo $local_code already exist at $local 
+	open $local
+	exit 2
+fi
+
+#echo downloading img:$url to $local_code
 curl -s -o $local_code $url
 open $local_code
 
 exit 2
+
+
