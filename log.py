dfbc9c864c74bb50d71344683ff4d81c2cf73a76
xiaoshenke <1205914137@qq.com>
2019-04-07 23:33:43 +0800

    update and fix some bug

--- log.py
+++ log.py
@@ -5,18 +5,25 @@ from cons import INN_RUNNING
 from util.time_util import today
 
 import logging, sys, os
-#logging.basicConfig(stream=sys.stderr,level=logging.DEBUG,filemode='a')
-logging.basicConfig(stream=sys.stdout,level=logging.DEBUG,filemode='a')
+#logging.basicConfig(stream=sys.stdout,level=logging.DEBUG,filemode='a')
 logger = logging.getLogger('stock')
-
-logger.setLevel(logging.DEBUG)
-#logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+fh = logging.StreamHandler(sys.stdout)
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
 
 f_logger = logging.getLogger('fstock')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-fh = logging.FileHandler("%s%s.csv"%(INN_RUNNING,str(today())))
-fh.setLevel(logging.INFO)
+fh = logging.FileHandler("%s%s.log"%(INN_RUNNING,str(today())))
+fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
-
 f_logger.addHandler(fh)
+fh = logging.StreamHandler(sys.stdout)
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+f_logger.addHandler(fh)
+f_logger.setLevel(logging.DEBUG)
 
+#f_logger.warn("hihihi")
