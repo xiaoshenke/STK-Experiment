d6b979e6de85b86931a9d6f2e7270f419317223c
xiaoshenke <1205914137@qq.com>
2019-04-08 19:06:13 +0800

    update

--- log.py
+++ log.py
@@ -5,13 +5,13 @@ from cons import INN_RUNNING
 from util.time_util import today
 
 import logging, sys, os
-#logging.basicConfig(stream=sys.stdout,level=logging.DEBUG,filemode='a')
+logging.basicConfig(stream=sys.stdout,level=logging.DEBUG,filemode='a')
 logger = logging.getLogger('stock')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 fh = logging.StreamHandler(sys.stdout)
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
-logger.addHandler(fh)
+#logger.addHandler(fh)
 logger.setLevel(logging.DEBUG)
 
 f_logger = logging.getLogger('fstock')
@@ -23,7 +23,5 @@ f_logger.addHandler(fh)
 fh = logging.StreamHandler(sys.stdout)
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
-f_logger.addHandler(fh)
+#f_logger.addHandler(fh)
 f_logger.setLevel(logging.DEBUG)
-
-#f_logger.warn("hihihi")
