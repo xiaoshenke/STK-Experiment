7516ffaeb32399b2261971bfda9a688444db0746
xiaoshenke <1205914137@qq.com>
2019-05-04 15:30:59 +0800

    add features.stk_shakes,add engine.handler.downstp_analyzer

--- log.py
+++ log.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # coding=utf-8
-
 from cons import INN_RUNNING
 from util.time_util import today
 
@@ -8,14 +7,11 @@ import logging, sys, os
 logging.basicConfig(stream=sys.stdout,level=logging.DEBUG,filemode='a')
 logger = logging.getLogger('stock')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-fh = logging.StreamHandler(sys.stdout)
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-#logger.addHandler(fh)
 logger.setLevel(logging.DEBUG)
 
 f_logger = logging.getLogger('fstock')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+# 配置文件路径
 fh = logging.FileHandler("%s%s.log"%(INN_RUNNING,str(today())))
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
