372b4e5f2f067a1674b53f81a45ea0c3252e3d0e
xiaoshenke <1205914137@qq.com>
2019-07-17 17:48:36 +0800

    fix python virtualenv issure when scheduled in crontab

--- sh/gene_report.sh
+++ sh/gene_report.sh
@@ -2,6 +2,10 @@
 
 export PYTHONPATH=/home/xiaoshenke100/STK-Experiment:$PYTHONPATH
 
+cd /home/xiaoshenke100
+source env1/bin/activate
+cd /home/xiaoshenke100/STK-Experiment
+
 echo generate report...
-nohup /usr/bin/python report/daily_generator.py >>nohup.out 2>&1 &
+nohup python report/daily_generator.py >>nohup.out 2>&1 &
 
--- sh/start.sh
+++ sh/start.sh
@@ -3,10 +3,14 @@
 # WSL: when scheduled by crontab,must specify PYTHONPATH
 export PYTHONPATH=/home/xiaoshenke100/STK-Experiment:$PYTHONPATH
 
+cd /home/xiaoshenke100
+source env1/bin/activate
+cd /home/xiaoshenke100/STK-Experiment
+
 echo start standby...
 # WSL: when scheduled by crontab,must specify >>nohup.out,otherwise no log
-nohup /usr/bin/python inn_strategy/standby_cli.py schedule-standby-and-merge >>nohup.out 2>&1 &
+nohup python inn_strategy/standby_cli.py schedule-standby-and-merge >>nohup.out 2>&1 &
 
 echo start engine...
-nohup /usr/bin/python engine/cli.py start-engine >>nohup.out 2>&1 &
+nohup python engine/cli.py start-engine >>nohup.out 2>&1 &
 
