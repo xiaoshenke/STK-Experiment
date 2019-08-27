5bc5742ac612814349e626abb56634ae3fbf6b8f
xiaoshenke <1205914137@qq.com>
2019-08-27 16:39:51 +0800

    update rapid_up_tracing.py

--- sh/gene_report.sh
+++ sh/gene_report.sh
@@ -2,9 +2,9 @@
 
 export PYTHONPATH=/home/xiaoshenke100/STK-Experiment:$PYTHONPATH
 
-cd /home/xiaoshenke100
-source env1/bin/activate
-cd /home/xiaoshenke100/STK-Experiment
+#cd /home/xiaoshenke100
+#source env1/bin/activate
+#cd /home/xiaoshenke100/STK-Experiment
 
 echo generate report...
 nohup python report/daily_generator.py >>nohup.out 2>&1 &
--- sh/start.sh
+++ sh/start.sh
@@ -9,8 +9,11 @@ cd /home/xiaoshenke100/STK-Experiment
 
 echo start standby...
 # WSL: when scheduled by crontab,must specify >>nohup.out,otherwise no log
-nohup python inn_strategy/standby_cli.py schedule-standby-and-merge >>nohup.out 2>&1 &
+#nohup python inn_strategy/standby_cli.py schedule-standby-and-merge >>nohup.out 2>&1 &
+
+nohup python inn_strategy/standby_cli.py schedule_standby_and_merge >standby.log 2>&1 &
 
 echo start engine...
-nohup python engine/cli.py start-engine >>nohup.out 2>&1 &
+#nohup python engine/cli.py start-engine >>nohup.out 2>&1 &
+nohup python engine/main_engine.py >>nohup.out 2>&1 &
 
