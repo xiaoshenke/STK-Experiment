e98b7ffa569b97b8435455befec8f26e9098456b
xiaoshenke <1205914137@qq.com>
2019-08-28 20:07:49 +0800

    fix bug in hl_fixed_util.py

--- sh/gene_report.sh
+++ sh/gene_report.sh
@@ -2,7 +2,7 @@
 
 # when scheduled by crontab,must specify PYTHONPATH
 export PYTHONPATH=/home/xiaoshenke100/STK-Experiment:$PYTHONPATH
-export: PYTHONPATH=/Users/wuxian/Desktop/STK-Experiment:$PYTHONPATH
+export PYTHONPATH=/Users/wuxian/Desktop/STK-Experiment:$PYTHONPATH
 
 #cd /home/xiaoshenke100
 #source env1/bin/activate
