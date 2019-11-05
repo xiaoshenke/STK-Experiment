895ec9a8c19d54cdb86914a6244481948c13a552
xiaoshenke <1205914137@qq.com>
2019-11-05 14:22:31 +0800

    integrate upgrated-df-merger to dm_job.py,engine.py

--- util/time_util.py
+++ util/time_util.py
@@ -10,10 +10,6 @@ def unix_to_str(t):
 def get_month_start(month,year=2018):
 	return '%d-%02d-01'%(year,month)
 
-#def get_month_start(day):
-#	day = parse_date_str(day)
-#	return '%d-%02d-01'%(day.year,day.month)
-
 def get_month_end(day):
 	day = parse_date_str(day)
 	wday,monthRange = calendar.monthrange(day.year, day.month)
@@ -47,5 +43,4 @@ def get_cur_timestamp(ct=-1):
 	return time_stamp
 
 if __name__ == "__main__":
-	print get_month_end('2018-02-03')
 	print get_cur_timestamp()
