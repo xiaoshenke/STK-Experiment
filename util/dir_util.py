d4ae547873f4adc659f92147efe5e18d7501b435
xiaoshenke <1205914137@qq.com>
2019-12-20 22:27:01 +0800

    update eva_stager,stage_node_processer

--- util/dir_util.py
+++ util/dir_util.py
@@ -32,6 +32,9 @@ def get_apply_dir(day=''):
 def get_evaluate_dir(day=''):
 	return "%s/evaluate/"%get_daily_dir(day)
 
+def get_df_merger_source_dir(day=''):
+	return "%s/df_merger_source/"%get_daily_dir(day)
+
 def get_filter_source_dir(day=''):
 	return "%s/filter_source/"%get_daily_dir(day)
 
@@ -58,7 +61,7 @@ def get_csv_path():
 	return CSV_PATH
 
 def init_dirs(day=''):
-	dirs = [get_daily_dir,get_apply_dir,get_evaluate_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_realtime_report_dir,get_codes_dir]
+	dirs = [get_daily_dir,get_apply_dir,get_evaluate_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_realtime_report_dir,get_codes_dir,get_df_merger_source_dir]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
