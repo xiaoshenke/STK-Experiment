5297277e9b77e20eb4cedfa3be5c9d5d415d9aee
xiaoshenke <1205914137@qq.com>
2020-02-13 00:06:38 +0800

    update index_tracing(50%)

--- util/dir_util.py
+++ util/dir_util.py
@@ -59,6 +59,9 @@ def get_cal_manager_source_dir(day=''):
 def get_queryable_source_dir(day=''):
 	return '%s/queryable_source/'%get_daily_dir(day)
 
+def get_tracing_dir(day=''):
+	return '%s/tracing/'%get_daily_dir(day)
+
 def get_stage_source_dir(day=''):
 	return "%s/stage_source/"%get_daily_dir(day)
 
@@ -73,7 +76,7 @@ def get_csv_path():
 	return CSV_PATH
 
 def init_dirs(day=''):
-	dirs = [get_daily_dir,get_apply_dir,get_evaluate_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_realtime_report_dir,get_codes_dir,get_df_merger_source_dir,get_manual_report_dir,get_scheduler_report_dir,get_shape_report_dir,get_open2_report_dir]
+	dirs = [get_daily_dir,get_apply_dir,get_evaluate_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_realtime_report_dir,get_codes_dir,get_df_merger_source_dir,get_manual_report_dir,get_scheduler_report_dir,get_shape_report_dir,get_open2_report_dir,get_tracing_dir]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
