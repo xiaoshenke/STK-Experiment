0689ad8d3a97d428c8e053203c38d221a4fa02a3
xiaoshenke <1205914137@qq.com>
2020-02-16 21:44:05 +0800

    add xls_dfastr_tracing(60% finish)

--- log.py
+++ log.py
@@ -51,6 +51,9 @@ cal_hist_codes_report_logger = get_common_daily_logger('cal_hist_report','hist_c
 # hit.xls_stage.hit_all_xls的盘面全版块信息,当前主要是涨停表现
 all_xls_logger = get_common_daily_logger('all_xls','all_xls')
 
+# xls dfastr tracing的日志
+xls_dfastr_tracing_logger = get_common_daily_logger('xls_dfastr','xls_dfastr')
+
 # 单次file刷新时的算子组中的stage-数据,用于挖掘优秀个股买点
 stage_minus_logger = get_common_daily_logger('stage_minus','stage-')
 
