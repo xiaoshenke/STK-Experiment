a9a1b4d3ec86444a09365576474a569d60b8f441
xiaoshenke <1205914137@qq.com>
2020-03-18 17:59:16 +0800

    xls_tracing_observer integrate to engine

--- log.py
+++ log.py
@@ -37,38 +37,14 @@ def get_common_daily_logger(appender,save_name,day=''):
 # event logger
 event_logger = get_common_daily_logger('event','event')
 
-# 单次file刷新时的算子组日志,记录了实际上cal manager所有发生的计算
-cals_report_logger = get_common_daily_logger('cals_report','cals_report')
-
-# 单次file刷新时,这个file对应的股票池数据的表现,当前主要是涨停表现
-cal_file_codes_report_logger = get_common_daily_logger('file_codes_report','file_codes_report')
-
-# cal manager计算出来过的历史结果股票数据的表现,当前主要是涨停表现
-cal_hist_codes_report_logger = get_common_daily_logger('cal_hist_report','hist_codes_report')
-
-# hit.xls_stage.hit_all_xls的盘面全版块信息,当前主要是涨停表现
-all_xls_logger = get_common_daily_logger('all_xls','all_xls')
+# xls-tracing-observer日志
+xls_tracing_observer_logger = get_common_daily_logger('xls_tracing_observer','xls_tracing_observer')
 
 # xls stage traing日志
 xls_stage_tracing_logger = get_common_daily_logger('xls_stage','xls_stage')
 
-# xls dfastr tracing的日志
-xls_dfastr_tracing_logger = get_common_daily_logger('xls_dfastr','xls_dfastr')
-
-# 单次file刷新时的算子组中的stage-数据,用于挖掘优秀个股买点
-stage_minus_logger = get_common_daily_logger('stage_minus','stage-')
-
-# hot xls的log,实质当前是被我用来做hit算子HIT_XLS_EFFECT_NORMAL_1记录操作用的
-hot_xls_logger = get_common_daily_logger('hot_xls','hot_xls')
-
-# hit process的日志
-hprocesser_logger = get_common_daily_logger('hit_processer','hit_processer')
-
 # codes merger日志
 codes_merger_logger = get_common_daily_logger('codes_merger','codes_merger')
-
-# market configer日志
-mkt_configer_logger = get_common_daily_logger('market_configer','market_configer')
 
 # dyna scheduler日志
 dyna_scheduler_logger = get_common_daily_logger('dyna_scheduler','dyna_scheduler')
@@ -78,12 +54,6 @@ xls_scheduler_logger = get_common_daily_logger('xls_scheduler','xls_scheduler')
 
 # shape scheduler调度日志
 shape_scheduler_logger = get_common_daily_logger('shape_scheduler','shape_scheduler')
-
-# open2 scheduler调度日志
-open2_scheduler_logger = get_common_daily_logger('open2_scheduler','open2_scheduler')
-
-# opening_xls scheduler调度日志
-opening_xls_scheduler_logger = get_common_daily_logger('opening_xls','opening_xls')
 
 # xls reporter 日志
 xls_reporter_logger = get_common_daily_logger('xls_reporter','xls_reporter')
@@ -125,36 +95,5 @@ def update_flogger(day=''):
 # 当日期变化的时候,调一下这个函数
 update_flogger()
 
-HIT_PROCESSER = 'hit_processer'
-
-HIT_HOT_XLS = 'hit_hot_xls'
-
-INN_EVA_LOG_NAME="inn_eva"
-
-INN_TYPE_GROUPED_EVA = 'type_grouped_eva'
-
-INN_CAL_SCHEDULE = "cal_schedule"
-
-INN_CAL_MANAGER = 'cal_manager'
-
-INN_DOWNLOADER_NAME="inn_downloader"
-
 INN_SERVER = "inn_server"
 
-INN_CODES_BUILDER = "codes_builder"
-
-INN_PRESCHEDULER = "prescheduler"
-
-INN_MARKET = 'market'
-
-INN_RAPID_UP_TRACING = 'rapid_up_tracing'
-
-INN_XLS_DOWNLOADER = 'xls_downloader'
-
-INN_LAST_TOP = 'last_top'
-
-INN_UPSTP_HOT = 'upstp_hot'
-
-INN_RAPID_XLS = 'rapid_xls'
-
-CAL_INTEGRATION = 'cal_integration'
