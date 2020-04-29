d0619d4cb0dc3194a3783b33fcce87b9a7f1cdef
xiaoshenke <1205914137@qq.com>
2020-04-29 15:31:49 +0800

    remove strategy.sc

--- cons.py
+++ cons.py
@@ -4,6 +4,9 @@
 SERVER_PORT = 1234
 SH_DOWNLOADER_SERVER_PORT = 1235
 
+# 创业板涨跌幅限制
+CHUANGYE_10_TO_20 = False
+
 CUR_PATH="/Users/wuxian/Desktop/STK-Experiment"
 CSV_PATH = "%s/data/csv_data/"%CUR_PATH
 XLS_PATH = "%s/data/xls_data/"%CUR_PATH
