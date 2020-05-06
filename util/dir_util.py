7474af7ba55f4e2d5a7016d3f1b382983eebf3e0
xiaoshenke <1205914137@qq.com>
2020-05-06 12:56:07 +0800

    engine.daily_reporter support noon mode

--- util/dir_util.py
+++ util/dir_util.py
@@ -80,6 +80,9 @@ def get_report_dir(day=''):
 def get_report2_dir(day=''):
 	return "%s/report2/"%get_daily_dir(day)
 
+def get_noon_report_dir(day=''):
+	return '%s/noon_report/'%get_daily_dir(day)
+
 def get_yidong_dir(day=''):
 	return "%s/yidong/"%get_daily_dir(day)
 
@@ -90,7 +93,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_df_merger_source_dir,get_manual_report_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_report2_dir,get_yidong_dir,get_prebuild_dir,get_hot_xls_dir]
+	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_df_merger_source_dir,get_manual_report_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_report2_dir,get_yidong_dir,get_prebuild_dir,get_hot_xls_dir,get_noon_report_dir]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
