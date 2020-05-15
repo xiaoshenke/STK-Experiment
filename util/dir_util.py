78d4929c7610cdf81ca0bb7102d3b1c7b4db003a
xiaoshenke <1205914137@qq.com>
2020-05-15 15:10:23 +0800

    clever_strategy support cross-cache feature,see in newhigh2.py

--- util/dir_util.py
+++ util/dir_util.py
@@ -56,6 +56,10 @@ def get_filter_source_dir(day=''):
 def get_create_source_dir(day=''):
 	return "%s/create_source/"%get_daily_dir(day)
 
+# 处理cross+inner类型的strategy的cache路径
+def get_create_cache_dir(day=''):
+	return "%s/create_cache/"%get_daily_dir(day)
+
 def get_dfa_source_dir(day=''):
 	return '%s/dfa_source/'%get_daily_dir(day)
 
@@ -93,7 +97,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_df_merger_source_dir,get_manual_report_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_report2_dir,get_yidong_dir,get_prebuild_dir,get_hot_xls_dir,get_noon_report_dir]
+	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_df_merger_source_dir,get_manual_report_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_report2_dir,get_yidong_dir,get_prebuild_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
