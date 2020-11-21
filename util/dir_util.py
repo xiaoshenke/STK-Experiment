36de87b0ed4f6c34cd11fd88225f39cb679c45ef
xiaoshenke <1205914137@qq.com>
2020-11-22 00:59:57 +0800

    add eva.trd_sum_eva,trd_minus_eva,update engine.stage.market.hot_xls_stager

--- util/dir_util.py
+++ util/dir_util.py
@@ -30,6 +30,10 @@ def get_xls_dir(day=''):
 def get_xls_stage_dir(day=''):
 	return '%s/xls_stage/'%get_daily_dir(day)
 
+# engine.stage.market的cache
+def get_market_stage_dir(day=''):
+	return '%s/market_stage/'%get_daily_dir(day)
+
 def get_realtime_report_dir(day=''):
         return "%s/realtime_report/"%get_daily_dir(day)
 
@@ -122,7 +126,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_env_dir,get_advise_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir]
+	dirs = [get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_env_dir,get_advise_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
