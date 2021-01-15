47dae86e873221d41f21b339e67112a6004cea40
xiaoshenke <1205914137@qq.com>
2021-01-16 00:24:24 +0800

    add engine.report.pllan_reporter

--- engine/report/xls/dir_util.py
+++ engine/report/xls/dir_util.py
@@ -4,6 +4,7 @@
 # 获取xls,type组合的实际路径,
 def get_dir_by(day,time_str,xls,type):
 	from util.dir_util import get_xls_report_dir
+	#type = '%s*%s'%(xls.split('*')[0],type)
 	return '%s/%s/%s/%s/'%(get_xls_report_dir(day),time_str.replace(':',''),xls,type)
 
 if __name__ == "__main__":
