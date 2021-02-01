df665b9e29abf5a31cc86e1a61f7014cc6cc1844
xiaoshenke <1205914137@qq.com>
2021-02-01 12:27:21 +0800

    update engine.scheduler.manual_planer

---
+++ sh/open_dir_codes.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Usage: sh/open_dir_codes.sh dir [code1,code2,...]
+
+if [ $# -eq 0 ]
+then
+	echo Usage: sh/open_dir_codes.sh dir [code1,code2,...]
+	exit 2
+fi
+
+dir=$1
+has_codes=0
+codes=''
+if [ $# -eq 2 ]
+then
+	has_codes=1
+	codes=$2
+fi
+
+# if no codes,just open dir/*.jpg
+if [ $has_codes -eq 0 ]
+then
+	echo open $dir/*.jpg
+	open $dir/*.jpg
+	exit 0
+fi
+
+codes=(${codes//,/ })
+# gather all open command string
+cmd=''
+for code in ${codes[@]}
+do
+	#echo open $dir/$code*.jpg
+	#open $dir/$code*.jpg
+	if [ -f "$dir/$code.d.jpg" ]
+	then
+		cmd="$cmd $dir/$code.d.jpg $dir/$code.f.jpg"
+	fi
+done
+
+#echo open $cmd
+open $cmd
