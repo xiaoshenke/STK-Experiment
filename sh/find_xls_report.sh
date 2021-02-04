379cb899f59673f76ab8f59182f6cbddb2602eac
xiaoshenke <1205914137@qq.com>
2021-02-05 00:18:23 +0800

    fix some bug in xls_reporter

---
+++ sh/find_xls_report.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# usage sh/find_xls_report --day [day] a b c
+
+day=`date +'%Y-%m-%d'`
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+greps=""
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	*)
+		if [ ${#greps} -eq 0 ]
+		then
+			greps=$1
+		else
+			greps="$greps,$1"
+		fi
+		;;
+	esac
+	shift
+done
+
+
+if [ ${#greps} -eq 0 ]
+then
+	echo find ../stk_daily/$day/xls_report/
+	exit 0
+fi
+
+greps=(${greps//,/ })	
+grep_cmd=''
+for grep in ${greps[@]}
+do
+	if [ ${#grep_cmd} -eq 0 ]
+	then
+		grep_cmd="grep $grep"
+	else
+		grep_cmd="$grep_cmd|grep $grep"
+	fi
+	# not support more than 2 greps
+	break
+done
+
+echo $grep_cmd
+echo "find ../stk_daily/$day/xls_report/|$grep_cmd"
+
+# fixme: not support more than 2 greps
+# hard to fix...
+find ../stk_daily/$day/xls_report/|$grep_cmd
