e505abce946df13eca6e3a8b0e7407ca64aeb26b
xiaoshenke <1205914137@qq.com>
2021-05-26 22:57:59 +0800

    update engine.plan.builded_buyer_rest,add run_build.sh

---
+++ sh/run_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo Usage:sh/run_build.sh day
+fi
+
+day=$1
+
+echo nohup python engine/crontab/build/build_job.py run $day >>build.log 2>&1 &
+nohup python engine/crontab/build/build_job.py run $day >>build.log 2>&1 &
+
