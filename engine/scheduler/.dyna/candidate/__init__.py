e3ca7582144160556dbe9b3b1264440ebd09017b
xiaoshenke <1205914137@qq.com>
2021-06-16 16:37:58 +0800

    market-observer add trade-time-change support,remove engine.scheduler.dyna,shape,preview

--- engine/scheduler/.dyna/candidate/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# coding=utf-8
-
-# 用于在盘中根据market找到xls
-# 其中marketed_candidate的逻辑为,根据当前market的entity找到符合该entity的某种dfastr的xls[版块]
-#	那么这个逻辑和engine.dfastr.xls包的功能强相关,见engine/dfastr/xls/marketed_dfastr.py
-
