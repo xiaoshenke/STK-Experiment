58894f84be6a9f04745752d55fa46adea456bb8c
xiaoshenke <1205914137@qq.com>
2021-08-12 22:02:15 +0800

    deep_observer add deep-top logic

---
+++ sh/cp_apply.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# usage sh/cp_apply.sh --day [day] abc aa:bb:cc dd:ee:ff
+
+day=`date +'%Y-%m-%d'`
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+if [ $# -ne 3 ]
+then
+	echo Usage:sh/cp_apply.sh --day [day] abc aa:bb:cc dd:ee:ff
+	exit 2
+fi
+
+type=$1
+t1=$2
+t2=$3
+
+# @ATTENTION: $day\_
+echo cp ../stk_daily/$day/apply/$type-$day\_$t1.csv ../stk_daily/$day/apply/$type-$day\_$t2.csv
+cp ../stk_daily/$day/apply/$type-$day\_$t1.csv ../stk_daily/$day/apply/$type-$day\_$t2.csv
+
