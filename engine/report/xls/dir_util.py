94c78e731544e5298f6d63265c93bca4e1205d80
xiaoshenke <1205914137@qq.com>
2021-12-14 19:34:36 +0800

    fast-buyer support fix-timestr

--- engine/report/xls/dir_util.py
+++ engine/report/xls/dir_util.py
@@ -2,9 +2,12 @@
 # coding=utf-8
 
 # 获取xls,type组合的实际路径,
-def get_dir_by(day,time_str,xls,type):
+def get_dir_by(day,time_str,xls,type,fix=0.0):
 	from util.dir_util import get_xls_report_dir
 	#type = '%s*%s'%(xls.split('*')[0],type)
+	if fix > 0:
+		return '%s/%s-FIX%s/%s/%s/'%(get_xls_report_dir(day),time_str.replace(':',''),fix,xls,type)
+
 	return '%s/%s/%s/%s/'%(get_xls_report_dir(day),time_str.replace(':',''),xls,type)
 
 if __name__ == "__main__":
