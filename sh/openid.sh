f2b8897c623db198a1c39e576e28638fcd0a20b7
xiaoshenke <1205914137@qq.com>
2021-12-21 15:29:43 +0800

    add engine.caop.zhz500.sub-buy.py

--- sh/openid.sh
+++ sh/openid.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+mode="jpg"
+
+# extract parameters
+while [ -n "$1" ]
+do
+	case "$1" in
+	-mode)
+		mode=$2
+		shift
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
 #echo "$@"
 if [ $# -ne 1 ]
 then
@@ -9,6 +26,6 @@ fi
 
 id=$1
 
-echo python realtime/query_cli.py open $id
-python realtime/query_cli.py open $id
+echo python realtime/query_cli.py open $id --open_mode $mode
+python realtime/query_cli.py open $id --open_mode $mode
 
