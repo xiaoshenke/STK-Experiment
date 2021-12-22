3cb56410acf725fd65f65c72d17d60124be82c58
xiaoshenke <1205914137@qq.com>
2021-12-22 13:36:16 +0800

    add tool.tempate-tool

--- util/dir_util.py
+++ util/dir_util.py
@@ -16,6 +16,9 @@ def get_daily_dir(day=''):
 def get_caop_dir(day=''):
 	return '%s/caop/'%get_daily_dir(day)
 
+def get_template_dir(day=''):
+	return '%s/template/'%get_daily_dir(day)
+
 def get_crontab_dir(day=''):
 	return '%s/crontab/'%get_daily_dir(day)
 
@@ -143,7 +146,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_env_dir,get_advise_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_crontab_dir,get_buyer_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir ]
+	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_shape_report_dir,get_tracing_dir,get_dfa_source_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_env_dir,get_advise_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_crontab_dir,get_buyer_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir,get_template_dir ]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
