3b49a4dfd085791e39c737b72326d24dec2ee901
xiaoshenke <1205914137@qq.com>
2021-12-30 16:06:11 +0800

    add engine.observe,run-over first sub-node,see pool-observe.py

---
+++ engine/observe/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# 新版的observe,定位为实盘的各个组件观察器,并提供各种交互功能
+
+# 和observer包的区别: observer是一个被动组件,当前一般仅用于显示一下[其他组件计算出的]结果数据.
+# 和tracing包的区别: 定位和tracing几乎一致,但是我更喜欢observe这个单词,我觉得语义上比tracing更加准确,tracing给人的感觉是仅仅观察一些简单的标的,而observe既可以观察简单的指标,也可以观察一些深度指标,并且tracing似乎不包含交互的语义
+
