a1e14670333d7a7850be0f9609306f70c1ad23a1
xiaoshenke <1205914137@qq.com>
2022-02-08 13:23:13 +0800

    update engine.caop.observe.code.buy

---
+++ sh/realtime/mimic_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# usage sh/mimic_all.sh [mimic]
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+mimic=1
+if [ $# -eq 1 ]
+then
+	mimic=$1
+fi
+
+echo python realtime/observe/stocks.py set --mimic $mimic
+python realtime/observe/stocks.py set --mimic $mimic
+
+echo python realtime/observe/pool.py set --mimic $mimic
+python realtime/observe/pool.py set --mimic $mimic
+
+echo python realtime/observe/longhu.py set --mimic $mimic
+python realtime/observe/longhu.py set --mimic $mimic
+
+echo python realtime/observe/market.py set --mimic $mimic
+python realtime/observe/market.py set --mimic $mimic
+
