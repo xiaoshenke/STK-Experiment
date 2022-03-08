fb624504cb0416c0fdd87ecfef76c3b5fa517e5d
xiaoshenke <1205914137@qq.com>
2022-03-08 12:43:14 +0800

    set-up engine.stage.pool framework

--- util/dir_util.py
+++ util/dir_util.py
@@ -61,6 +61,10 @@ def get_xls_stage_dir(day=''):
 def get_market_stage_dir(day=''):
 	return '%s/market_stage/'%get_daily_dir(day)
 
+# @engine.stage.pool
+def get_pool_stage_dir(day=''):
+	return '%s/pool_stage/'%get_daily_dir(day)
+
 def get_shizhan_dir(day=''):
 	return '%s/shizhan/'%(get_daily_dir(day))
 
@@ -120,7 +124,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_tracing_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_crontab_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir,get_template_dir,get_observe_dir,get_listener_dir,get_buyer_dir,get_dadan_dir ]
+	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_tracing_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_pool_stage_dir,get_crontab_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir,get_template_dir,get_observe_dir,get_listener_dir,get_buyer_dir,get_dadan_dir ]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
