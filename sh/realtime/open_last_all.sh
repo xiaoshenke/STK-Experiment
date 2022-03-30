54458075f831f6c808fa6c4b6532ce1c77195534
xiaoshenke <1205914137@qq.com>
2022-03-30 14:13:11 +0800

    finish first-version engine.buyer.small-buyer.py

--- sh/realtime/open_last_all.sh
+++ sh/realtime/open_last_all.sh
@@ -4,8 +4,8 @@
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
-echo python realtime/observe/upstp.py open_last
-python realtime/observe/upstp.py open_last
+echo python realtime/observe/market.py open_last
+python realtime/observe/market.py open_last
 
 echo python realtime/observe/stocks.py open_last
 python realtime/observe/stocks.py open_last
@@ -13,11 +13,17 @@ python realtime/observe/stocks.py open_last
 echo python realtime/observe/pool.py open_last
 python realtime/observe/pool.py open_last
 
-echo python realtime/observe/longhu.py open_last
-python realtime/observe/longhu.py open_last
+echo python realtime/observe/upstp.py open_last
+python realtime/observe/upstp.py open_last
 
-echo python realtime/observe/market.py open_last
-python realtime/observe/market.py open_last
+#echo python realtime/observe/longhu.py open_last
+#python realtime/observe/longhu.py open_last
+
+echo python realtime/observe/bind.py open_last
+python realtime/observe/bind.py open_last
 
 echo python realtime/observe/xls.py open_last
 python realtime/observe/xls.py open_last
+
+
+
