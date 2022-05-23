ff3094b323ef9faccf4e51e046dbe8c112e95115
xiaoshenke <1205914137@qq.com>
2022-05-23 21:01:22 +0800

    update engine.xls-opener

--- sh/realtime/mimic_all.sh
+++ sh/realtime/mimic_all.sh
@@ -36,6 +36,10 @@ python realtime/observe/xls.py set --mimic $mimic
 echo python realtime/observe/bind.py set --mimic $mimic
 python realtime/observe/bind.py set --mimic $mimic
 
+echo python realtime/observe/longhu.py set --mimic $mimic
+python realtime/observe/longhu.py set --mimic $mimic
+
+
 echo python realtime/buyer/observe_cli.py set --mimic $mimic
 python realtime/buyer/observe_cli.py set --mimic $mimic
 
--- sh/start_reporter_server.sh
+++ sh/start_reporter_server.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code 0
+mimic=1
+if [ $# -eq 1 ]
+then
+	mimic=$1
+fi
 
-nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code 0 >>reporter.log 2>&1 &
+echo nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code $mimic
+
+nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code $mimic >>reporter.log 2>&1 &
