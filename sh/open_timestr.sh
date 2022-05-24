0895e9ea503cc4af3115730dad5ca0a74f650ffc
xiaoshenke <1205914137@qq.com>
2022-05-24 20:39:35 +0800

    update engine.observe.change add codes-node.py

---
+++ sh/open_timestr.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mode="jpg"
+
+# extract parameters
+while [ -n "$1" ]
+do
+	case "$1" in
+	-mode|--mode)
+		mode=$2
+		shift
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+#echo "$@"
+if [ $# -lt 1 ]
+then
+	echo usage: sh/open_timestr.sh aa:bb:cc [xls]
+	exit 0
+fi
+
+time_str=$1
+xls=""
+if [ $# -eq 2 ]
+then
+	xls=$2
+	echo python realtime/query_cli.py open_timestr $time_str --xls $xls --open_mode $mode
+	python realtime/query_cli.py open_timestr $time_str --xls $xls --open_mode $mode
+else
+	echo python realtime/query_cli.py open_timestr $time_str --open_mode $mode
+	python realtime/query_cli.py open_timestr $time_str --open_mode $mode
+fi
+
