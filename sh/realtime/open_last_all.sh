3ca60fd1bf59f5d2e286e0c972d0c0eca0d9c344
xiaoshenke <1205914137@qq.com>
2022-06-19 22:36:14 +0800

    add engine.observe.dapiao-observe

--- sh/realtime/open_last_all.sh
+++ sh/realtime/open_last_all.sh
@@ -1,44 +1,121 @@
 #!/bin/bash
-# usage sh/open_last_all.sh
+# usage sh/open_last_all.sh [-day aaaa-bb-cc] [-time_str xx:yy:zz] [mode]
+
+day=`date +'%Y-%m-%d'`
+time_str=`date +'%H:%M:%S'`
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+mode='default'
+if [ $# -eq 1 ]
+then
+	mode=$1
+fi
 
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
-echo python realtime/observe/market.py open_last
-python realtime/observe/market.py open_last
+echo 当前支持的mode为chaozuo,market,pool,default,dapiao
 
-echo python realtime/observe/stocks.py open_last
-python realtime/observe/stocks.py open_last
+if [[ $mode == "chaozuo" ]]
+then
+	echo python realtime/observe/high.py open_last
+	python realtime/observe/high.py open_last --day $day --time_str $time_str
 
-#echo python realtime/observe/longhu.py open_last
-#python realtime/observe/longhu.py open_last
+	echo python realtime/observe/chaozuo.py open_last
+	python realtime/observe/chaozuo.py open_last --day $day --time_str $time_str
 
-#echo python realtime/observe/chaozuo.py open_last
-#python realtime/observe/chaozuo.py open_last
+	#echo python realtime/observe/stocks.py open_last
+	#python realtime/observe/stocks.py open_last --day $day --time_str $time_str
+elif [[ $mode == "dapiao" ]]
+then
+	echo python realtime/observe/zhz500.py open_last
+	python realtime/observe/zhz500.py open_last --day $day --time_str $time_str
 
-#echo python realtime/observe/pool.py open_last
-#python realtime/observe/pool.py open_last
+	echo python realtime/observe/dapiao.py open_last
+	python realtime/observe/dapiao.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/upstp.py open_last
-python realtime/observe/upstp.py open_last
+	echo python realtime/observe/stocks.py open_last
+	python realtime/observe/stocks.py open_last --day $day --time_str $time_str
+elif [[ $mode == "pool" ]]
+then
+	echo python realtime/observe/mline.py open_last
+	python realtime/observe/mline.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/bind.py open_last
-python realtime/observe/bind.py open_last
+	echo python realtime/observe/pools.py open_last
+	python realtime/observe/pools.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/xls.py open_last
-python realtime/observe/xls.py open_last
+	echo python realtime/observe/upstp.py open_last
+	python realtime/observe/upstp.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/mline.py open_last
-python realtime/observe/mline.py open_last
+	echo python realtime/observe/bind.py open_last
+	python realtime/observe/bind.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/change.py open_last
-python realtime/observe/change.py open_last
+	echo python realtime/observe/change.py open_last
+	python realtime/observe/change.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/pools.py open_last
-python realtime/observe/pools.py open_last
+elif [[ $mode == "market" ]]
+then
+	echo python realtime/observe/market.py open_last
+	python realtime/observe/market.py open_last --day $day --time_str $time_str
 
-echo python realtime/observe/dig.py open_last
-python realtime/observe/dig.py open_last
+	echo python realtime/observe/change.py open_last
+	python realtime/observe/change.py open_last --day $day --time_str $time_str
 
-#echo python realtime/buyer/observe_cli.py open_last
-#python realtime/buyer/observe_cli.py open_last
+	echo python realtime/observe/stocks.py open_last
+	python realtime/observe/stocks.py open_last --day $day --time_str $time_str
+else
+	echo python realtime/observe/market.py open_last
+	python realtime/observe/market.py open_last
+
+	echo python realtime/observe/stocks.py open_last
+	python realtime/observe/stocks.py open_last
+
+	#echo python realtime/observe/longhu.py open_last
+	#python realtime/observe/longhu.py open_last
+
+	#echo python realtime/observe/chaozuo.py open_last
+	#python realtime/observe/chaozuo.py open_last
+
+	#echo python realtime/observe/pool.py open_last
+	#python realtime/observe/pool.py open_last
+
+	echo python realtime/observe/upstp.py open_last
+	python realtime/observe/upstp.py open_last
+
+	echo python realtime/observe/bind.py open_last
+	python realtime/observe/bind.py open_last
+
+	echo python realtime/observe/xls.py open_last
+	python realtime/observe/xls.py open_last
+
+	echo python realtime/observe/mline.py open_last
+	python realtime/observe/mline.py open_last
+
+	echo python realtime/observe/change.py open_last
+	python realtime/observe/change.py open_last
+
+	echo python realtime/observe/pools.py open_last
+	python realtime/observe/pools.py open_last
+
+	echo python realtime/observe/dig.py open_last
+	python realtime/observe/dig.py open_last
+
+	#echo python realtime/buyer/observe_cli.py open_last
+	#python realtime/buyer/observe_cli.py open_last
+fi
