e03b7e35472358f44d34038ef5edc0bf5313aeee
xiaoshenke <1205914137@qq.com>
2022-07-14 18:21:00 +0800

    update cut_high3,cut_longhu3,cut_shouban3

--- sh/start_pans.sh
+++ sh/start_pans.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
-echo "依次起standby,node updater,xls tracing,market tracing,change observe,mline observe"
+# Usage: sh/start_pans.sh [mode]
 
-nohup python inn_strategy/standby_cli.py schedule_standby_and_merge2 --enable_merger false >>standby.log 2>&1 &
+mode='default'
+if [ $# -eq 1 ]
+then
+	mode=$1
+fi
 
-# 轻量级运行default advisor(下载热门版块数据) 
-nohup python engine/advise/node/cli.py start_node_engine_mode update >>node.log 2>&1 &
+if [[ $mode == "light" ]]
+then
+	echo "[轻量级模式]:依次起standby,node updater,xls tracing,market tracing"
 
-# 轻量级运行xls stage tracing
-nohup python engine/tracing/xls/cli.py start_tracing_engine_mode >>tracing.log 2>&1 &
+	echo nohup python inn_strategy/standby_cli.py schedule_standby_and_merge2 --enable_merger false 
+	nohup python inn_strategy/standby_cli.py schedule_standby_and_merge2 --enable_merger false >>standby.log 2>&1 &
 
-# 轻量级运行market stage tracing
-nohup python engine/tracing/market/cli.py start_tracing_engine_mode >>tracing.market.log 2>&1 &
+	echo nohup python engine/advise/node/cli.py start_node_engine_mode update
+	# 轻量级运行default advisor(下载热门版块数据) 
+	nohup python engine/advise/node/cli.py start_node_engine_mode update >>node.log 2>&1 &
 
-nohup python engine/observe/change/cli.py start_engine_mode >>observe.change.log 2>&1 &
+	# 轻量级运行xls stage tracing
+	echo nohup python engine/tracing/xls/cli.py start_tracing_engine_mode
+	nohup python engine/tracing/xls/cli.py start_tracing_engine_mode >>tracing.log 2>&1 &
 
-nohup python engine/observe/mline/cli.py start_engine_mode >>observe.mline.log 2>&1 &
+	# 轻量级运行market stage tracing
+	echo nohup python engine/tracing/market/cli.py start_tracing_engine_mode
+	nohup python engine/tracing/market/cli.py start_tracing_engine_mode >>tracing.market.log 2>&1 &
 
-nohup python engine/observe/xls/cli.py start_engine_mode >>observe.xls.log 2>&1 &
+elif [[ $mode == "default" ]]
+then
+	echo "依次起standby,node updater,xls tracing,market tracing,change observe,mline observe"
 
-nohup python engine/observe/chaozuo/cli.py start_engine_mode >>observe.chaozuo.log 2>&1 &
+	nohup python inn_strategy/standby_cli.py schedule_standby_and_merge2 --enable_merger false >>standby.log 2>&1 &
 
-nohup python engine/observe/pools/cli.py start_engine_mode >>observe.pools.log 2>&1 &
+	# 轻量级运行default advisor(下载热门版块数据) 
+	nohup python engine/advise/node/cli.py start_node_engine_mode update >>node.log 2>&1 &
 
-nohup python engine/scheduler/buyer/cli.py start_engine_mode >>scheduler.buyer.log 2>&1 &
+	# 轻量级运行xls stage tracing
+	nohup python engine/tracing/xls/cli.py start_tracing_engine_mode >>tracing.log 2>&1 &
+
+	# 轻量级运行market stage tracing
+	nohup python engine/tracing/market/cli.py start_tracing_engine_mode >>tracing.market.log 2>&1 &
+
+	nohup python engine/observe/change/cli.py start_engine_mode >>observe.change.log 2>&1 &
+
+	nohup python engine/observe/mline/cli.py start_engine_mode >>observe.mline.log 2>&1 &
+
+	#nohup python engine/observe/xls/cli.py start_engine_mode >>observe.xls.log 2>&1 &
+
+	nohup python engine/observe/longhu/cli.py start_engine_mode --mimic_open true >>observe.longhu.log 2>&1 &
+
+	nohup python engine/observe/chaozuo/cli.py start_engine_mode >>observe.chaozuo.log 2>&1 &
+
+	nohup python engine/observe/pools/cli.py start_engine_mode >>observe.pools.log 2>&1 &
+
+	nohup python engine/scheduler/buyer/cli.py start_engine_mode >>scheduler.buyer.log 2>&1 &
+else
+        echo 不支持$mode
+fi
+
+
