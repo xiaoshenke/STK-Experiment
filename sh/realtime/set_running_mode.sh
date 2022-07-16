78880bda673b09e8c74a5ecd063c7742e8e1e0c9
xiaoshenke <1205914137@qq.com>
2022-07-16 12:06:14 +0800

    add prop.xls.detail-property

--- sh/realtime/set_running_mode.sh
+++ sh/realtime/set_running_mode.sh
@@ -10,3 +10,18 @@ fi
 echo python realtime/observe/change.py set --running_mode $mode
 python realtime/observe/change.py set --running_mode $mode
 
+echo python realtime/observe/market.py set --running_mode $mode
+python realtime/observe/market.py set --running_mode $mode
+
+echo python realtime/observe/pools.py set --running_mode $mode
+python realtime/observe/pools.py set --running_mode $mode
+
+echo python realtime/observe/upstp.py set --running_mode $mode
+python realtime/observe/upstp.py set --running_mode $mode
+
+echo python realtime/observe/xls.py set --running_mode $mode
+python realtime/observe/xls.py set --running_mode $mode
+
+echo python realtime/buyer/observe_cli.py set --running_mode $mode
+python realtime/buyer/observe_cli.py set --running_mode $mode
+
