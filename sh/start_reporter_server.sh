00d61f1e4a7f8153c096df89640351a62265ae5a
xiaoshenke <1205914137@qq.com>
2022-07-18 16:51:13 +0800

    add engine.stock.stock-chooser.py

--- sh/start_reporter_server.sh
+++ sh/start_reporter_server.sh
@@ -6,6 +6,8 @@ then
 	mimic=$1
 fi
 
+export PYTHONUNBUFFERED=1
+
 echo nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code $mimic
 
 nohup python engine/report/plan/cli.py start_plan_reporter_engine_mode --mimic_open_code $mimic >>reporter.log 2>&1 &
