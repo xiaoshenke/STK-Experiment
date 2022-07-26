eed822c29651e7e57eb719f3b6b4fdc87b18b959
xiaoshenke <1205914137@qq.com>
2022-07-26 11:15:21 +0800

    remove engine.plan

--- sh/realtime/set_running_mode.sh
+++ sh/realtime/set_running_mode.sh
@@ -22,6 +22,7 @@ python realtime/observe/upstp.py set --running_mode $mode
 echo python realtime/observe/xls.py set --running_mode $mode
 python realtime/observe/xls.py set --running_mode $mode
 
-echo python realtime/buyer/observe_cli.py set --running_mode $mode
-python realtime/buyer/observe_cli.py set --running_mode $mode
+
+#echo python realtime/buyer/observe_cli.py set --running_mode $mode
+#python realtime/buyer/observe_cli.py set --running_mode $mode
 
