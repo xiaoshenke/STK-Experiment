eb497a63011d9f3ac176ad6ffc12057cd6849dc5
xiaoshenke <1205914137@qq.com>
2022-11-14 11:42:54 +0800

    update scheduler.buyer.register-recorder,update dyna-cli

---
+++ sh/start_buyer_scheduler.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PYTHONUNBUFFERED=1
+
+echo nohup python engine/scheduler/buyer/cli.py start_engine_mode >>scheduler.buyer.log 2>&1 &
+nohup python engine/scheduler/buyer/cli.py start_engine_mode >>scheduler.buyer.log 2>&1 &
+
