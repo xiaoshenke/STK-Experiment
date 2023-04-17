d1b041deed1e51a85f2a5c2e23c328e6b5d12bf2
xiaoshenke <1205914137@qq.com>
2023-04-17 13:16:01 +0800

    add candi.clever-code-types,add caop.code-types package

--- util/dir_util.py
+++ util/dir_util.py
@@ -102,6 +102,9 @@ def get_create_source_dir(day=''):
 def get_candi_source_dir(day=''):
 	return '%s/candi_source/'%get_daily_dir(day)
 
+def get_code_types_source_dir(day=''):
+	return '%s/code_types_source/'%get_daily_dir(day)
+
 def get_stocks_source_dir(day=''):
 	return '%s/stocks_source/'%get_daily_dir(day)
 
@@ -137,7 +140,7 @@ def get_csv_path():
 	return "%s/data/csv_data/"%get_cur_dir()
 
 def init_dirs(day=''):
-	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_tracing_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_code_stage_dir,get_pool_stage_dir,get_crontab_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir,get_template_dir,get_observe_dir,get_listener_dir,get_buyer_dir,get_dadan_dir,get_tail_dir,get_png_dir,get_stocks_source_dir ]
+	dirs = [ get_daily_dir,get_apply_dir,get_filter_source_dir,get_fenshi_wholecodes_dir,get_report_dir,get_open_dir,get_create_source_dir,get_stage_source_dir,get_queryable_source_dir,get_tracing_dir,get_xls_report_dir,get_hot_xls_dir,get_noon_report_dir,get_create_cache_dir,get_candi_source_dir,get_plan_dir,get_suma_dir,get_xls_stage_dir,get_market_stage_dir,get_code_stage_dir,get_pool_stage_dir,get_crontab_dir,get_shizhan_dir,get_evaluate_dir,get_mline_dir,get_mline_xls_list_dir,get_caop_dir,get_template_dir,get_observe_dir,get_listener_dir,get_buyer_dir,get_dadan_dir,get_tail_dir,get_png_dir,get_stocks_source_dir,get_code_types_source_dir ]
 	dirs = [ dir(day) for dir in dirs ]
 	import os
 	for dir in dirs:
