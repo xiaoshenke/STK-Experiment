1a5a35170146f21d780a7450f7ab00ebcae71c14
xiaoshenke <1205914137@qq.com>
2023-05-23 12:30:18 +0800

    add stage.market.amount-stager

--- sh/cmds2.sh
+++ sh/cmds2.sh
@@ -7,7 +7,9 @@ time_str=#
 mode=#
 
 type=''
+front=''
 
+now=0
 while [ -n "$1" ]
 do 
         case "$1" in 
@@ -28,7 +30,13 @@ do
                 exit 1
                 ;;
         *)
-		type=$1
+		if [ $now -eq 0 ]
+		then
+			type=$1
+		else
+			front=$1
+		fi
+		now=$now+1
                 ;;
         esac
         shift
@@ -37,6 +45,12 @@ done
 if [ $# -eq 1 ]
 then
 	type=$1
+fi
+
+if [ $# -eq 2 ]
+then
+	type=$1
+	front=$2
 fi
 
 if [ ${#type} -eq 0 ]
@@ -49,6 +63,13 @@ fi
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
-echo python realtime/cmds_cli.py buyer_cmds $type --day $day --time_str $time_str --mode $mode
-python realtime/cmds_cli.py buyer_cmds $type --day $day --time_str $time_str --mode $mode
+if [ ${#front} -eq 0 ]
+then
+	echo python realtime/cmds_cli.py buyer_cmds $type --day $day --time_str $time_str --mode $mode
+	python realtime/cmds_cli.py buyer_cmds $type --day $day --time_str $time_str --mode $mode
+	exit 1
+fi
+
+echo python realtime/caop/front.py cmds --day $day --time_str $time_str --mode $mode $type $front
+python realtime/caop/front.py cmds --day $day --time_str $time_str --mode $mode $type $front
 
