3abf27135db3287d8578007b23f5ee1c3f32a0a0
xiaoshenke <1205914137@qq.com>
2023-08-01 13:16:33 +0800

    add strategy.alias.upbound-2,shangy-2,near-newhigh-1

--- sh/start_code_types_observe.sh
+++ sh/start_code_types_observe.sh
@@ -2,6 +2,22 @@
 
 export PYTHONUNBUFFERED=1
 
-echo python engine/observe/code_types/cli.py start_engine_mode
-nohup python engine/observe/code_types/cli.py start_engine_mode >>observe.code_types.log 2>&1 &
+operate=#
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-operate | --operate)
+		shift
+		operate=$1
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+echo python engine/observe/code_types/cli.py start_engine_mode --operate $operate
+nohup python engine/observe/code_types/cli.py start_engine_mode --operate $operate >>observe.code_types.log 2>&1 &
 
