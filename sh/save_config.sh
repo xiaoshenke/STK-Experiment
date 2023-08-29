fd8f2c4bf47f15dc58cbab0e24ff36bb72a41730
xiaoshenke <1205914137@qq.com>
2023-08-29 20:18:24 +0800

    add strategy.alias.not-ignores.py

---
+++ sh/save_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# save realtime/realtime.properties to ../stk_daily/$day 
+
+# usage sh/save_config.sh [day]
+
+day=`date +'%Y-%m-%d'`
+if [ $# -eq 1 ]
+then
+	day=$1
+fi
+
+echo cp realtime/realtime.properties ../stk_daily/$day/
+cp realtime/realtime.properties ../stk_daily/$day/
+
