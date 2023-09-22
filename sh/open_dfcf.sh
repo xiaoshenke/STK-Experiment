29dafa4b3ca9fb180c7d5b618ee6874ac293c4a9
xiaoshenke <1205914137@qq.com>
2023-09-22 11:12:43 +0800

    add strategy.wrap.huazi-strategy

--- sh/open_dfcf.sh
+++ sh/open_dfcf.sh
@@ -19,6 +19,14 @@ then
 	exit 1
 fi
 
+if [[ ${code:0:2} == "2." ]]
+then
+	url=https://quote.eastmoney.com/zz/$code.html
+	echo going to open url:$url ...
+	open $url
+	exit 1
+fi
+
 url=http://quote.eastmoney.com/bk/90.$code.html
 echo going to open url:$url ...
 open $url
