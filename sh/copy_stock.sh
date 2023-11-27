6e8b24ff2db4eebc7f7eb0d55d71b59d91933ef4
xiaoshenke <1205914137@qq.com>
2023-11-27 13:03:53 +0800

    add strategy.breaks,add strategy.xls-chaoduan-1,update engine.report.copy-util add-beizheng-support

--- sh/copy_stock.sh
+++ sh/copy_stock.sh
@@ -78,6 +78,9 @@ code=$1
 if [ $is_index -eq 1 ] || [[ ${code:0:2} == "60" ]] || [[ ${code:0:2} == "68" ]]
 then
 	code=sh$code
+elif [ $is_index -eq 1 ] || [[ ${code:0:1} == "4" ]] || [[ ${code:0:1} == "8" ]]
+then
+	code=bj$code
 else
 	code=sz$code
 fi
