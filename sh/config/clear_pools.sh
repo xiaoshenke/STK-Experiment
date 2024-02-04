9b775d3172c52a97e7d4c100d9e620dee9e3b711
xiaoshenke <1205914137@qq.com>
2024-02-05 01:01:22 +0800

    add taoli.market.branches-node,add crontab.build.download-xiweis-job.py

--- sh/config/clear_pools.sh
+++ sh/config/clear_pools.sh
@@ -4,8 +4,23 @@
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
+force=0
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-force | --force | --froce)
+		shift
+		force=$1
+		;; 
+	*)
+		;;
+	esac
+	shift
+done
+
 echo 用于清空配置文件中的所有pool
 
-echo python realtime/properties_cli.py clear_pools
-python realtime/properties_cli.py clear_pools
+echo python realtime/properties_cli.py clear_pools --force $force
+python realtime/properties_cli.py clear_pools --force $force
 
