c429c456a5fcff53f5c8c4a9751ef4f803f93271
xiaoshenke <1205914137@qq.com>
2024-03-05 18:50:35 +0800

    add caop.code-type.flush.jingjia-flush,add eva.alias.jj-low0-1,add eva.ruozq-eva.py

---
+++ sh/start_mao_observe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Usage: sh/start_mao_observe.sh [xls]
+
+xls=db31
+if [ $# -eq 1 ]
+then
+	xls=$1
+fi
+
+export PYTHONUNBUFFERED=1
+
+echo nohup python engine/observe/mao/cli.py start_engine_mode --xls $xls
+nohup python engine/observe/mao/cli.py start_engine_mode --mimic_open false --xls $xls >>observe.mao.log 2>&1 &
+
