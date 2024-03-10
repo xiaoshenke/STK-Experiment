3e22569b7d0048b2455acad04eeb5f259f253514
xiaoshenke <1205914137@qq.com>
2024-03-10 23:37:29 +0800

    add plan.builded.shape-plan

--- sh/start_buyer_scheduler.sh
+++ sh/start_buyer_scheduler.sh
@@ -2,6 +2,12 @@
 
 export PYTHONUNBUFFERED=1
 
-echo nohup python engine/scheduler/buyer/cli.py start_engine_mode >>scheduler.buyer.log 2>&1 &
-nohup python engine/scheduler/buyer/cli.py start_engine_mode --debug 0 >>scheduler.buyer.log 2>&1 &
+mimic=0
+if [ $# -eq 1 ]
+then
+	mimic=$1
+fi
+
+echo nohup python engine/scheduler/buyer/cli.py start_engine_mode --mimic_open $mimic >>scheduler.buyer.log 2>&1 &
+nohup python engine/scheduler/buyer/cli.py start_engine_mode --debug 0 --mimic_open $mimic >>scheduler.buyer.log 2>&1 &
 
