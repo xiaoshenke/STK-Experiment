df44fa87b47c4410cfd314cf3e570989820c540f
xiaoshenke <1205914137@qq.com>
2024-03-11 20:05:44 +0800

    add engine.code-type.observe-util,update realtime.code-types.flush-cli

--- sh/start_buyer_scheduler.sh
+++ sh/start_buyer_scheduler.sh
@@ -8,6 +8,6 @@ then
 	mimic=$1
 fi
 
-echo nohup python engine/scheduler/buyer/cli.py start_engine_mode --mimic_open $mimic >>scheduler.buyer.log 2>&1 &
+echo python engine/scheduler/buyer/cli.py start_engine_mode --mimic_open $mimic 
 nohup python engine/scheduler/buyer/cli.py start_engine_mode --debug 0 --mimic_open $mimic >>scheduler.buyer.log 2>&1 &
 
