b03e44f5c2edec973d51a0dd6a0217b4e5c74a75
xiaoshenke <1205914137@qq.com>
2024-05-16 13:33:21 +0800

    add plan.market.zhudao-node.py

--- sh/realtime/set_running_mode.sh
+++ sh/realtime/set_running_mode.sh
@@ -37,6 +37,9 @@ python realtime/observe/upstp.py set --running_mode $mode
 echo python realtime/observe/chaoduan.py set --running_mode $mode
 python realtime/observe/chaoduan.py set --running_mode $mode
 
+echo python realtime/observe/buyer.py set --running_mode $mode
+python realtime/observe/buyer.py set --running_mode $mode
+
 #echo python realtime/observe/xls.py set --running_mode $mode
 #python realtime/observe/xls.py set --running_mode $mode
 
