885704ad64c8869a655d4bc0751a91a397fa0484
xiaoshenke <1205914137@qq.com>
2024-06-10 16:07:45 +0800

    add caop.pan.pan-template.py

---
+++ sh/open_pan.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo ATTENTION: OPEN-PAN CLI
+
+day=#
+type=#
+
+now=0
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+ 	-type | --type)
+		shift
+		type=$1
+		;;
+	-help | --help)
+		echo usage sh/open_pan.sh [--day abc] [--type xyz]
+                exit 1
+                ;;
+        *)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			day=$1
+		elif [ $now -eq 1 ]
+		then
+			type=$1
+		fi
+		declare -i now=$now+1
+                ;;
+	esac
+	shift
+done
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+echo python engine/caop/pan/cli.py open --day $day --type $type
+python engine/caop/pan/cli.py open --day $day --type $type
+
