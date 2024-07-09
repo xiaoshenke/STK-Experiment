40cdf3c7d26dbe72f24ffd35e6045793b493ae7b
xiaoshenke <1205914137@qq.com>
2024-07-09 13:54:04 +0800

    add caop.code-type.flush-buy,add eva.alias.jj-highs-1.py

--- sh/flush_cli.sh
+++ sh/flush_cli.sh
@@ -7,39 +7,39 @@ time_str=#
 mode=#
 
 type=''
-flush_type='simple'
+flush_type='buy'
 front_type=#
 ignore_cache=0
 
 now=0
 while [ -n "$1" ]
 do 
-        case "$1" in 
-        -day | --day)
-                shift
-                day=$1
-                ;;
-        -time_str | --time_str)
-                shift
-                time_str=$1
-                ;;
-        -mode | --mode)
-                shift
-                mode=$1
-                ;;
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
 	-eva | --eva | --front | -front | --front_type | -front_type)
-                shift
-                front_type=$1
-                ;;
+		shift
+		front_type=$1
+		;;
 	-ignore_cache | --ignore_cache)
-                shift
-                ignore_cache=$1
-                ;;
-        -help | --help)
+		shift
+		ignore_cache=$1
+		;;
+	-help | --help)
 		echo usage sh/realtime/flush_cli.sh [--day abc] [--time_str xyz] [--mode aaa ] type
-                exit 1
-                ;;
-        *)
+		exit 1
+		;;
+	*)
 		# set value to type|flush_type by now-flag
 		if [ $now -eq 0 ]
 		then
@@ -51,9 +51,9 @@ do
 			front_type=$1
 		fi
 		declare -i now=$now+1
-                ;;
-        esac
-        shift
+		;;
+	esac
+	shift
 done
 
 if [ $# -eq 1 ]
@@ -81,12 +81,20 @@ if [[ $flush_type == "simple" ]] || [[ $flush_type == "simple_pull" ]] || [[ $fl
 then
 	echo python realtime/flush_cli.py flush $type --day $day --time_str $time_str --mode $mode --flush_type $flush_type --ignore_cache $ignore_cache
 	python realtime/flush_cli.py flush $type --day $day --time_str $time_str --mode $mode --flush_type $flush_type --ignore_cache $ignore_cache
+
+elif [[ $flush_type =~ "buy" ]]
+then
+	echo python realtime/flush_cli.py flush_buy $type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+	python realtime/flush_cli.py flush_buy $type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+
 elif [[ $flush_type =~ "pull" ]]
 then
 	echo python realtime/flush_cli.py pull $type --flush_type $flush_type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
 	python realtime/flush_cli.py pull $type --flush_type $flush_type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+
 else
 	echo python realtime/flush_cli.py flush_subs $type $flush_type --front_type $front_type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
 	python realtime/flush_cli.py flush_subs $type $flush_type --front_type $front_type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+
 fi
 
