2934c5190d40e1de3d2e49fd7521dd6d7db2890b
xiaoshenke <1205914137@qq.com>
2024-08-29 20:29:35 +0800

    add observe.code-types.flush-all2-node(pan-mode-flush-all)

--- sh/start_code_types_observe.sh
+++ sh/start_code_types_observe.sh
@@ -5,13 +5,17 @@ export PYTHONUNBUFFERED=1
 # update 2024-01-16: trend->jpg
 # update 2024-08-29: jpg->trend
 operate=trend
-
+mode="default"
 while [ -n "$1" ]
 do 
 	case "$1" in 
 	-operate | --operate)
 		shift
 		operate=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
 		;;
 	*)
 		break
@@ -20,6 +24,6 @@ do
 	shift
 done
 
-echo python engine/observe/code_types/cli.py start_engine_mode --operate $operate
-nohup python engine/observe/code_types/cli.py start_engine_mode --operate $operate >>observe.code_types.log 2>&1 &
+echo python engine/observe/code_types/cli.py start_engine_mode --operate $operate --mode $mode
+nohup python engine/observe/code_types/cli.py start_engine_mode --operate $operate --mode $mode >>observe.code_types.log 2>&1 &
 
