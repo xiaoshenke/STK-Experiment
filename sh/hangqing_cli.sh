5ca9a45e81c331d70a14210f593a881aebedbba9
xiaoshenke <1205914137@qq.com>
2024-09-08 18:18:02 +0800

    update candi.fronts.pulls-fronts-candi

--- sh/hangqing_cli.sh
+++ sh/hangqing_cli.sh
@@ -88,6 +88,11 @@ then
 	echo python realtime/hangqing_cli.py maoding $type maoding:yz --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
 	python realtime/hangqing_cli.py maoding $type maoding:yz --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
 
+elif [[ $hq_type == "zhuxian" ]] || [[ $hq_type == "zhuxians" ]]
+then
+	echo python realtime/hangqing_cli.py zhuxian $type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+	python realtime/hangqing_cli.py zhuxian $type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache
+
 elif [[ $hq_type =~ "jinji" ]]
 then
 	echo python realtime/hangqing_cli.py jinji $type $hq_type --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache 
