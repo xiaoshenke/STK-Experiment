56fde51a1222c18b7c4320dd463e3d23c99be00c
xiaoshenke <1205914137@qq.com>
2024-10-07 20:30:14 +0800

    add cons.OPEN_IS_0925 and relative-logic

--- cons.py
+++ cons.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # coding=utf-8
 
+# 09:25而不是09:30作为open时区 见<2024-10-07 应对09:25~30分变更为可交易时间政策做一下engine的更改>
+OPEN_IS_0925 = True
+
 SERVER_PORT = 1234
 PLAN_REPORTER_SERVER_PORT = 1222
 SH_DOWNLOADER_SERVER_PORT = 1236
