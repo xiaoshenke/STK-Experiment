06239cb9effc2003120ed2d8b54e5d6bde2d475b
xiaoshenke <1205914137@qq.com>
2025-01-03 15:56:00 +0800

    add observe.code-type.tmp-tool,add sh/list/add_tmp.sh,add candi.fronts.outxt-fronts-candi.py

---
+++ sh/list/add_tmp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+code_type=#
+desc=#
+
+now=0
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			code_type=$1
+		elif [ $now -eq 1 ]
+		then
+			desc=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python realtime/code_type/tmp_cli.py add $code_type --day $day --reason $desc
+python realtime/code_type/tmp_cli.py add $code_type --day $day --reason $desc
