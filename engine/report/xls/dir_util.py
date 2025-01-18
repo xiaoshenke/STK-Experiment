656c4d482622d9fb6df515e33acd5763e2547643
xiaoshenke <1205914137@qq.com>
2025-01-18 20:53:47 +0800

    add candi.subs.name-subs-candi,add strategy.alias.max-trds-1.py

--- engine/report/xls/dir_util.py
+++ engine/report/xls/dir_util.py
@@ -10,6 +10,14 @@ def get_dir_by(day,time_str,xls,type,fix=0.0):
 
 	return '%s/%s/%s/%s/'%(get_xls_report_dir(day),time_str.replace(':',''),xls,type)
 
+def get_dir_by_eva(day,time_str,xls,eva):
+	if time_str == '15:00:00':
+		from util.dir_util import get_report_dir
+		return '%s/%s/%s/'%(get_report_dir(day),xls,eva)
+
+	from util.dir_util import get_xls_report_dir
+	return '%s/%s/%s/%s/'%(get_xls_report_dir(day),time_str.replace(':',''),xls,eva)
+
 # return bool,xls,type,day,time_str
 def parse_dir(dir):
 	if not dir:
@@ -19,17 +27,34 @@ def parse_dir(dir):
 	if not dir.endswith('csv'):
 		dir = dir+'/'
 
-	if not 'xls_report' in dir:
+	# update 2025-01-18: 当前支持两种路径 xls-report|report
+	geshi = ''
+
+	if 'xls_report' in dir:
+		geshi = 'xls_report'
+	elif 'report' in dir:
+		geshi = 'report'
+	
+	if not geshi:
 		return False,'','','',''
 
 	# 处理//
 	dir = dir.replace('//','/')
-	idx = dir.index('xls_report')
-	
+	#idx = dir.index('xls_report')
+
+	idx = dir.index(geshi)	
+
 	day = dir[idx-1-10:idx-1]
-	idx2 = dir.find('/',idx+1)
-	time_str = dir[idx2+1:idx2+1+6]
-	time_str = '%s:%s:%s'%(time_str[0:2],time_str[2:4],time_str[4:6])
+
+	time_str = ''
+	idx2 = -1
+	if geshi == 'xls_report':
+		idx2 = dir.find('/',idx+1)
+		time_str = dir[idx2+1:idx2+1+6]
+		time_str = '%s:%s:%s'%(time_str[0:2],time_str[2:4],time_str[4:6])
+	elif geshi == 'report':
+		idx2 = idx
+		time_str = '15:00:00'
 
 	idx3 = dir.find('/',idx2+1)
 	idx4 = dir.find('/',idx3+1)
