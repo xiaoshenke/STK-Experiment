ae1d26d0934f1fa1720d525105e540db43f997ed
xiaoshenke <1205914137@qq.com>
2025-03-06 14:49:26 +0800

    add caop.manual.save.buyer-save-to.py

---
+++ sh/manual/buyer_cli.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+echo ATTENTION: MANUAL-BUYER-CMDS CLI
+
+day=#
+time_str=#
+mode='now'
+
+operate=''
+branch=#
+codes=#
+clear=0
+ignore_cache=1
+
+now=0
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-branch | --branch)
+		shift
+		branch=$1
+		;;
+        -clear | --clear)
+		shift
+		clear=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-ignore_cache | --ignore_cache)
+		shift
+		ignore_cache=$1
+		;;
+	-help | --help)
+		echo usage sh/manual/buyer_cli.sh [--day abc] [--time_str xyz] [--mode aaa ] type
+		exit 1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			operate=$1
+		elif [ $now -eq 1 ]
+		then
+			branch=$1
+			codes=$1
+		elif [ $now -eq 2 ]
+		then
+			branch=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+if [[ $operate == "saveto" ]] || [[ $operate == "save_to" ]]
+then
+
+	echo python realtime/manual_buyer_cli.py save_to $branch --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache --clear $clear
+	python realtime/manual_buyer_cli.py save_to $branch --day $day --time_str $time_str --mode $mode --ignore_cache $ignore_cache --clear $clear
+
+else
+	echo 当前不支持类型$operate,branch:$branch
+fi
+
