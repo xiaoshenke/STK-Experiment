704c33bbdc3ddb4265cdac69d4ac0ccb383bdd47
xiaoshenke <1205914137@qq.com>
2025-03-11 14:18:55 +0800

    update

--- sh/open_pan.sh
+++ sh/open_pan.sh
@@ -4,8 +4,10 @@ echo ATTENTION: OPEN-PAN CLI
 
 day=#
 
+# update 2025-03-10: 取消走读取逻辑,因为那样会让行为变的复杂
 # 先走一个读取逻辑,设置一下type的值 这个值是前一次打开text文件时记录的
-type=$(python realtime/properties_cli.py read_pan_type)
+#type=$(python realtime/properties_cli.py read_pan_type)
+type='default'
 
 now=0
 while [ -n "$1" ]
@@ -23,7 +25,7 @@ do
 		echo usage sh/open_pan.sh [--day abc] [--type xyz]
                 exit 1
                 ;;
-        *)
+	*)
 		# set value to type|day by now-flag
 		if [ $now -eq 0 ]
 		then
