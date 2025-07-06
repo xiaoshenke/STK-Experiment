88172a06dbcfadd1e1d2b0095a603c5bda15fbe2
xiaoshenke <1205914137@qq.com>
2025-07-06 19:15:09 +0800

    add observe.code-types.color-node,add engine.eva-str package

---
+++ sh/eva/add_eva_str.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+code_type=#
+desc=#
+name=#
+now=0
+mode='plan'
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-reason | --reason | -desc| --desc)
+		shift
+		desc=$1
+		;;
+	-name | --name)
+		shift
+		name=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			code_type=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/eva_str/reg_cli.py add $code_type --name $name --day $day --mode $mode --reason "$desc"
+python engine/eva_str/reg_cli.py add $code_type --name $name --day $day --mode $mode --reason "$desc" 
