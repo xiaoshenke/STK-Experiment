df7c51e0498cefbf98b2c2e53b327b3e8522753e
xiaoshenke <1205914137@qq.com>
2025-07-23 11:28:56 +0800

    add buy.backend.no-report-backend

--- sh/eva/cli.sh
+++ sh/eva/cli.sh
@@ -120,7 +120,7 @@ fi
 path=`pwd`
 export PYTHONPATH=$path:$PYTHONPATH
 
-echo python eva/cli.py do_eva --day $day --time_str $time_str --mode $mode $type $front_type --open $open --ignore_cache $ignore_cache --back $back_type --debug $debug --do_log $log --do_report $do_report --open_jpg $open_jpg --add $add --desc $desc --level $level
+echo python eva/cli.py do_eva --day $day --time_str $time_str --mode $mode $type $front_type --open 1 --do_log 1 [real] --open $open --do_log $log --ignore_cache $ignore_cache --back $back_type --debug $debug --do_report $do_report --open_jpg $open_jpg --add $add --desc $desc --level $level
 
 python eva/cli.py do_eva --day $day --time_str $time_str --mode $mode $type $front_type --ignore_cache $ignore_cache --open $open --back $back_type --debug $debug --do_log $log --do_report $do_report --open_txt 1 --open_jpg $open_jpg --add $add --desc $desc --level $level --silent $silent
 # --fake $fake
