bfb9b0aeca45b83fb76e38edfde4b00a62826f1d
xiaoshenke <1205914137@qq.com>
2025-08-25 20:41:57 +0800

    add some observe.buyer.template.xxxxx.template.properties

--- sh/template/list.sh
+++ sh/template/list.sh
@@ -4,5 +4,5 @@ echo 版块模版列表
 ls engine/observe/buyer/template/|grep xx_|grep [.]properties|sort
 
 echo ""
-echo 行情列表
+echo "行情列表 find engine/observe/buyer/template/|grep -v xx_|grep [.]properties"
 ls engine/observe/buyer/template/|grep -v xx_|grep [.]properties|sort
