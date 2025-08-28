16be6684a3b5c7e172bb195b40720b0f2c589ee1
xiaoshenke <1205914137@qq.com>
2025-08-28 16:59:50 +0800

    add sh/plan/run-xls-template.sh,add observe.buyer.template.xx-pull.template.properties,add plan.template.dapiao-pool.template.properties

---
+++ sh/plan/run_xls_template.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+xls=#
+template=#
+now=0
+time_str=#
+mode='plan'
+
+if [ $# -lt 2 ]
+then
+	echo Usage: sh/juben/run_template.sh template [--day ] 
+	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			xls=$1
+		elif [ $now -eq 1 ]
+		then
+			template=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/observe/plan/template_cli.py run_xls_template $xls $template --day $day --mode $mode --time_str $time_str
+python engine/observe/plan/template_cli.py run_xls_template $xls $template --day $day --mode $mode --time_str $time_str
+
