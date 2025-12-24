96e888c9679ceab106e00f0338740bf33d544162
xiaoshenke <1205914137@qq.com>
2025-12-24 11:28:34 +0800

    add eva-str.export.zhu.jj-top-evas,zao-top-evas,add observe.tracing.node.eva-str-node

---
+++ sh/tracing/run_eva_str.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+type=#
+now=0
+time_str=#
+mode='now'
+ignore_cache=0
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/tracing/run_codes.sh type [--day ] 
+	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-ignore_cache | --ignore_cache)
+		shift
+		ignore_cache=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			type=$1
+		elif [ $now -eq 1 ]
+		then
+			day=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/observe/tracing/run_cli.py run_codes eva_str:$type --day $day --mode $mode --time_str $time_str --ignore_cache $ignore_cache
+python engine/observe/tracing/run_cli.py run_codes eva_str:$type --day $day --mode $mode --time_str $time_str --ignore_cache $ignore_cache
