82dfa3dd9f956a4a28ff5667c37fd101317d6699
xiaoshenke <1205914137@qq.com>
2026-01-30 15:38:03 +0800

    add observe.buyer.buyer-recorder,add observe.buyer.buyer-runner,add sh/buyer/run-buyer.sh

---
+++ sh/buyer/add_buyer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+code_type=#
+desc=#
+name=#
+now=0
+mode='plan'
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-reason | --reason | -desc| --desc)
+		shift
+		desc=$1
+		;;
+	-name | --name)
+		shift
+		name=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			code_type=$1
+		elif [ $now -eq 1 ]
+		then 
+			desc=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/observe/buyer/reg_cli.py add $code_type --name $name --day $day --mode $mode --reason "$desc"
+python engine/observe/buyer/reg_cli.py add $code_type --name $name --day $day --mode $mode --reason "$desc" 
---
+++ sh/buyer/run_buyer.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+eva_str=#
+now=0
+add=0
+mode='now'
+operate='flush'
+time_str=#
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/eva/run_eva_str.sh eva_str [--day ] 
+      	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-operate | --operate)
+		shift
+		operate=$1
+		;;
+	-add | --add_codes | --add)
+		shift
+		add=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			eva_str=$1
+		elif [ $now -eq 1 ]
+		then
+			operate=$1
+		elif [ $now -eq 2 ]
+		then
+			day=$day
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/observe/buyer/runner/cli.py run $eva_str --day $day --time_str $time_str --mode $mode 
+python engine/observe/buyer/runner/cli.py run $eva_str --day $day --time_str $time_str --mode $mode 
