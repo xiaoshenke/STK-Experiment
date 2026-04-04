234cec696078340265402f54b3df289c4a3f331b
xiaoshenke <1205914137@qq.com>
2026-04-04 15:17:21 +0800

    add candi.node.use-pool-candi,update observe.juben.eva-node add set-realtime-xls,set-realtime-pool logic,add sh/hq/market_pull_analyze.sh

---
+++ sh/hq/market_pull_analyze.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+day=`date +'%Y-%m-%d'`
+time_str=#
+mode='now'
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	*)
+		;;
+	esac
+	shift
+done
+
+# 配置一下analyze pool模式
+echo sh/config/analyze_pool.sh 1
+sh/config/analyze_pool.sh 1
+
+echo sh/template/run_template.sh market_pull_analyze --day $day --mode $mode
+sh/template/run_template.sh market_pull_analyze --day $day --mode $mode
+
+
