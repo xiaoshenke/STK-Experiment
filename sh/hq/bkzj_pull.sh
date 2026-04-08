8c1b6f46a6fb2bc4f6e9b05b3ec6e8b756d37a77
xiaoshenke <1205914137@qq.com>
2026-04-08 14:32:49 +0800

    add candi.parser.realtime-builder.add sh/hq/bkzj-pull.sh

---
+++ sh/hq/bkzj_pull.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+day=`date +'%Y-%m-%d'`
+time_str=#
+mode='now'
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	*)
+		;;
+	esac
+	shift
+done
+
+echo sh/template/run_template.sh bkzj_pull --day $day --mode $mode
+sh/template/run_template.sh bkzj_pull --day $day --mode $mode
+
