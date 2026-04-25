5936394ff87d831d2f6e91d543f8db108fccb3ed
xiaoshenke <1205914137@qq.com>
2026-04-25 16:17:10 +0800

    add sh/codes/plan/analyze-dadan.sh

---
+++ sh/codes/plan/analyze_dadan.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/xls/plan/find_subs.sh xxx [day]
+	exit 2
+fi
+
+day=`date +'%Y-%m-%d'`
+time_str='0'
+mode='plan'
+
+xls='bkzj.cdadan:limit=15*find_xls'
+now=0
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			xls=$1
+		elif [ $now -eq 1 ]
+		then
+			day=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo sh/template/run_pool_template.sh $xls analyze_dadan --day $day --mode $mode
+sh/template/run_pool_template.sh $xls analyze_dadan --day $day --mode $mode
+
