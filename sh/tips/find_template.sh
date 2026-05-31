be5c097948c2d422cb6a9e7c03060b7a1c951554
xiaoshenke <1205914137@qq.com>
2026-05-31 13:50:20 +0800

    add observe.tips.template.safe.tips.properties

---
+++ sh/tips/find_template.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# 搜索当前所有可能的template文件
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+template=#
+now=0
+time_str=#
+mode='plan'
+force=0
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/tips/find_template.sh template [--day ]
+	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			template=$1
+		elif [ $now -eq 1 ]
+		then
+			day=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+cur_dir=/Users/wuxian/Desktop/STK-Experiment
+template_dir=$cur_dir/engine/observe/tips/template/
+caozuo_dir=/Users/wuxian/Desktop/stk_daily/$day/caozuo/
+
+result=$( ls $template_dir | grep tips.properties|grep $template )
+if [ ${#result} -eq 0 ]
+then
+	echo 尝试寻找tips模板文件 $template_dir 数据失败
+else
+	echo ""
+	echo "## tips模板文件 find $template_dir | grep tips.properties | grep $template"
+	echo 成功找到文件如下:
+	echo "$result"
+	echo ""
+fi
+
+result=$( ls $caozuo_dir |grep Run|grep Template| grep $template)
+if [ ${#result} -eq 0 ]
+then
+	echo 尝试寻找caozuo计算结果文件 $caozuo_dir 数据失败
+else
+	echo ""
+	echo "## caozuo计算结果文件 find $caozuo_dir |grep Run|grep Template| grep $template"
+	echo 成功找到文件如下:
+	echo "$result"
+	echo ""
+fi
+
+echo ""
+
---
+++ sh/tips/run_template.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+template=#
+template2=#
+now=0
+time_str=#
+mode='now'
+operate='flush'
+ignore_cache=1
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/tips/run_template.sh template [--day ] 
+	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-operate | --operate)
+		shift
+		operate=$1
+		;;
+	-ignore_cache | --ignore_cache | -ignore | --ignore)
+		shift
+		ignore_cache=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			template=$1
+		elif [ $now -eq 1 ]
+		then
+			#template2=$1
+			operate=$1
+		elif [ $now -eq 2 ]
+		then
+			day=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python realtime/observe/tips.py run_template $template --day $day --mode $mode --time_str $time_str --operate $operate
+python realtime/observe/tips.py run_template $template --day $day --mode $mode --time_str $time_str --operate $operate --ignore_cache $ignore_cache
