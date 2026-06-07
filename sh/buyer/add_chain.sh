30ffd72d3a36fa0bf5387973e6f844ae758af4bf
xiaoshenke <1205914137@qq.com>
2026-06-07 23:31:26 +0800

    add observe.buyer.register.chain-register,codes1-chain-parser,add sh/buyer/add-chain.sh

---
+++ sh/buyer/add_chain.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/buyer/add_chain.sh xx [reason|desc]
+	exit 2
+fi
+
+day=`date +'%Y-%m-%d'`
+code_type=#
+desc=#
+name=#
+now=0
+mode='now'
+print_run_msg=0
+flush=0
+force=0
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-flush | --flush)
+		shift
+		flush=$1
+		;;
+	-force | --force)
+		shift
+		force=$1
+		;;
+	-print_run_msg | --print_run_msg)
+		shift
+		print_run_msg=$1
+		;;
+	-reason | --reason | -desc| --desc)
+		shift
+		desc=$1
+		;;
+	-name | --name)
+		shift
+		name=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			code_type=$1
+		elif [ $now -eq 1 ]
+		then 
+			desc=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+echo python engine/observe/buyer/register/chain/reg_cli.py add "$code_type" --name $name --day $day --mode $mode --reason "$desc" --print_run_msg $print_run_msg --flush $flush --force $force
+
+python engine/observe/buyer/register/chain/reg_cli.py add "$code_type" --name $name --day $day --mode $mode --reason "$desc" --print_run_msg $print_run_msg --flush $flush --force $force
