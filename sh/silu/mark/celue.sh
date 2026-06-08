6ca1c05652b8374363ce48d5f11438d3d1776ff0
xiaoshenke <1205914137@qq.com>
2026-06-08 21:05:12 +0800

    add observe.buyer.chain.codes2_chain_parser,req_log1_chain_parser,add tips.template.baodie_hupan.tips.properties

---
+++ sh/silu/mark/celue.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# 记录有效的买点链路
+
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+silu=#
+detail='0'
+time_str=#
+now=0
+mode='now'
+xls=#
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/silu/mark/chain.sh silu [detail] 
+      	exit 2
+fi
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-reason | --reason | -desc| --desc)
+		shift
+		detail=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	-mode | --mode)
+		shift
+		mode=$1
+		;;
+	-xls | --xls)
+		shift
+		xls=$1
+		;;
+	*)
+		# set value to type|flush_type by now-flag
+		if [ $now -eq 0 ]
+		then
+			silu="$1"
+		elif [ $now -eq 1 ]
+		then
+			detail="$1"
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python tool/silu/reg_cli.py mark "$silu" --type "celue" --detail "$detail" --day $day --time_str $time_str --mode $mode --xls $xls
+python tool/silu/reg_cli.py mark "$silu" --type "celue" --detail "$detail" --day $day --time_str $time_str --mode $mode --xls $xls --level 3
+
