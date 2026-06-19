da3b570dcec5a8d406c1e23c2ac03b86d6648a60
xiaoshenke <1205914137@qq.com>
2026-06-19 11:51:30 +0800

    add strategy.piece.pos.high-list,qushi.up-list,yuanli.yuanli-two-list,add observe.tips.xx-basic-xls.tips.properties

--- sh/template/find_template.sh
+++ sh/template/find_template.sh
@@ -71,7 +71,7 @@ then
 	echo 尝试寻找$cur_dir/engine/observe/juben/template/ 下数据失败
 else
 	echo ""
-	echo "## 默认的模板文件 find $cur_dir/engine/observe/juben/template/ | grep $template"
+	echo "## 默认的模板文件 find engine/observe/juben/template/ | grep $template"
 	echo 成功找到文件如下:
 	echo "$result"
 	echo ""
