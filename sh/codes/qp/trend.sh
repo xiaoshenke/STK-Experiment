5618c7c0e84dace91d87be0278438272dbf3dabe
xiaoshenke <1205914137@qq.com>
2026-07-04 16:24:12 +0800

    add engine.recorder.properties_recorder,add engine.codes.recorder.qp_operate_recorder,add sh/codes/qp package

---
+++ sh/codes/qp/trend.sh
@@ -0,0 +1,6 @@
+
+# 尝试读取@engine/codes/recorder/qp_operate_recorder.py 是否有过数据 如果有 那么成功读取到对应的properties no,然后再从@engine/recorder/properties_recorder.py 中读取实际的properties 然后进行调度运行
+# 如果没有 那么打开默认的overall tips文件
+
+# TODO:
+
