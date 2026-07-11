8e6f609915927bdfa488c5b2da51225909332747
xiaoshenke <1205914137@qq.com>
2026-07-11 11:48:10 +0800

    update chain-recorder add query_ids_suma_info+query_by_id_and_gene_simple_info

--- sh/chain/list_all.sh
+++ sh/chain/list_all.sh
@@ -1,4 +1,42 @@
+#!/bin/bash
 
-# 打印所有的id列表
-# TODO:
+# Usage: sh/list/code_type.sh [day]
 
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+day=`date +'%Y-%m-%d'`
+time_str='0'
+now=0
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			day=$1
+		elif [ $now -eq 1 ]
+                then
+			time_str=$1
+		fi
+		;;
+	esac
+	shift
+done
+
+if [ $# -eq 1 ]
+then
+	day=$1
+fi
+
+echo python engine/recorder/chain/cli.py last --day $day --time_str $time_str
+python engine/recorder/chain/cli.py last --day $day --time_str $time_str
--- sh/chain/print_id.sh
+++ sh/chain/print_id.sh
@@ -1,3 +1,45 @@
+#!/bin/bash
 
-# 打印某个id的所有数据 TODO:
+# Usage: sh/list/code_type.sh [day]
 
+path=`pwd`
+export PYTHONPATH=$path:$PYTHONPATH
+
+if [ $# -lt 1 ]
+then
+	echo Usage: sh/chain/print_id.sh xx
+	exit 2
+fi
+
+day=`date +'%Y-%m-%d'`
+time_str='0'
+now=0
+id=-1
+
+while [ -n "$1" ]
+do 
+	case "$1" in 
+	-day | --day)
+		shift
+		day=$1
+		;;
+	-time_str | --time_str)
+		shift
+		time_str=$1
+		;;
+	*)
+		if [ $now -eq 0 ]
+		then
+			id=$1
+		elif [ $now -eq 1 ]
+                then
+			day=$1
+		fi
+		declare -i now=$now+1
+		;;
+	esac
+	shift
+done
+
+echo python engine/recorder/chain/cli.py query_id_and_gene_simple_info $id --day $day 
+python engine/recorder/chain/cli.py query_id_and_gene_simple_info $id --day $day
